2109567ccf3c295e4feb5e52acd5f38874f9410d
Martin Mühlemann <poelsi@gmail.com>
2021-04-15 20:45:41 +0200

    Support pattern and size constraints for emails

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/schema/EmailSchemaMapper.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/schema/EmailSchemaMapper.java
@@ -1,11 +1,31 @@
 package com.github.muehmar.gradle.openapi.generator.java.schema;
 
+import com.github.muehmar.gradle.openapi.generator.MappedSchema;
 import com.github.muehmar.gradle.openapi.generator.constraints.Constraints;
+import com.github.muehmar.gradle.openapi.generator.java.type.JavaType;
 import com.github.muehmar.gradle.openapi.generator.java.type.JavaTypes;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.swagger.v3.oas.models.media.EmailSchema;
 
-public class EmailSchemaMapper extends SimpleSchemaMapper<EmailSchema> {
+public class EmailSchemaMapper extends BaseSchemaMapper<EmailSchema> {
   public EmailSchemaMapper(JavaSchemaMapper nextMapper) {
-    super(EmailSchema.class, JavaTypes.STRING.withConstraints(Constraints.ofEmail()), nextMapper);
+    super(EmailSchema.class, nextMapper);
+  }
+
+  @Override
+  MappedSchema<JavaType> mapSpecificSchema(
+      String pojoKey,
+      String key,
+      EmailSchema schema,
+      PojoSettings pojoSettings,
+      JavaSchemaMapper chain) {
+
+    final Constraints patternConstraints = ConstraintsMapper.getPattern(schema);
+    final Constraints minAndMaxLengthConstraints = ConstraintsMapper.getMinAndMaxLength(schema);
+
+    final JavaType type =
+        JavaTypes.STRING.withConstraints(
+            Constraints.ofEmail().and(patternConstraints).and(minAndMaxLengthConstraints));
+    return MappedSchema.ofType(type);
   }
 }
