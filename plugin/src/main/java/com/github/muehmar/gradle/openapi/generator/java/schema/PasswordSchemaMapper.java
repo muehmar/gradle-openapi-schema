dd80f50501e7f8d38e4900772ac96ab95362a211
Martin Mühlemann <poelsi@gmail.com>
2021-04-15 20:50:28 +0200

    Support pattern and size constraints for passwords

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/schema/PasswordSchemaMapper.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/schema/PasswordSchemaMapper.java
@@ -1,10 +1,30 @@
 package com.github.muehmar.gradle.openapi.generator.java.schema;
 
+import com.github.muehmar.gradle.openapi.generator.MappedSchema;
+import com.github.muehmar.gradle.openapi.generator.constraints.Constraints;
+import com.github.muehmar.gradle.openapi.generator.java.type.JavaType;
 import com.github.muehmar.gradle.openapi.generator.java.type.JavaTypes;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.swagger.v3.oas.models.media.PasswordSchema;
 
-public class PasswordSchemaMapper extends SimpleSchemaMapper<PasswordSchema> {
+public class PasswordSchemaMapper extends BaseSchemaMapper<PasswordSchema> {
   public PasswordSchemaMapper(JavaSchemaMapper nextMapper) {
-    super(PasswordSchema.class, JavaTypes.STRING, nextMapper);
+    super(PasswordSchema.class, nextMapper);
+  }
+
+  @Override
+  MappedSchema<JavaType> mapSpecificSchema(
+      String pojoKey,
+      String key,
+      PasswordSchema schema,
+      PojoSettings pojoSettings,
+      JavaSchemaMapper chain) {
+
+    final Constraints patternConstraints = ConstraintsMapper.getPattern(schema);
+    final Constraints minAndMaxLengthConstraints = ConstraintsMapper.getMinAndMaxLength(schema);
+
+    final JavaType type =
+        JavaTypes.STRING.withConstraints(patternConstraints.and(minAndMaxLengthConstraints));
+    return MappedSchema.ofType(type);
   }
 }
---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/schema/PasswordSchemaMapperTest.java
@@ -0,0 +1,32 @@
+package com.github.muehmar.gradle.openapi.generator.java.schema;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ch.bluecare.commons.data.PList;
+import com.github.muehmar.gradle.openapi.generator.MappedSchema;
+import com.github.muehmar.gradle.openapi.generator.constraints.Constraints;
+import com.github.muehmar.gradle.openapi.generator.constraints.Pattern;
+import com.github.muehmar.gradle.openapi.generator.constraints.Size;
+import com.github.muehmar.gradle.openapi.generator.java.type.JavaType;
+import com.github.muehmar.gradle.openapi.generator.java.type.JavaTypes;
+import io.swagger.v3.oas.models.media.PasswordSchema;
+import org.junit.jupiter.api.Test;
+
+class PasswordSchemaMapperTest {
+
+  private static final PasswordSchemaMapper PASSWORD_SCHEMA_MAPPER = new PasswordSchemaMapper(null);
+
+  @Test
+  void
+      mapSchema_when_passwordSchemaWithPatternAndMinMaxLengthConstraints_then_correctJavaTypeAndConstraints() {
+    final PasswordSchema passwordSchema = new PasswordSchema();
+    passwordSchema.pattern("pattern").minLength(5).maxLength(50);
+    final MappedSchema<JavaType> mappedSchema =
+        PASSWORD_SCHEMA_MAPPER.mapSchema("pojoKey", "key", passwordSchema, null, null);
+    assertEquals(
+        JavaTypes.STRING.withConstraints(
+            Constraints.ofSize(Size.of(5, 50)).and(Constraints.ofPattern(new Pattern("pattern")))),
+        mappedSchema.getType());
+    assertEquals(PList.empty(), mappedSchema.getOpenApiPojos());
+  }
+}
