36d83a12ef2a2c0051d149e0f6b6e34cccf12023
Martin Mühlemann <poelsi@gmail.com>
2022-02-22 20:50:14 +0100

    Update example

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/inlineobject/TestInlineObject.java
@@ -0,0 +1,36 @@
+package com.github.muehmar.gradle.openapi.inlineobject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import OpenApiSchema.example.api.inlineobject.model.CustomerDto;
+import OpenApiSchema.example.api.inlineobject.model.CustomerInlineObjectAnotherObjectDto;
+import OpenApiSchema.example.api.inlineobject.model.CustomerInlineObjectDto;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+class TestInlineObject {
+
+  @Test
+  void customerDtoIsSuccessfullyGeneratedAndCanBeUsed() {
+    final CustomerInlineObjectAnotherObjectDto anotherObjectDto =
+        CustomerInlineObjectAnotherObjectDto.newBuilder()
+            .setKey("key")
+            .andAllOptionals()
+            .setData("data")
+            .build();
+    final CustomerInlineObjectDto customerInlineObjectDto =
+        CustomerInlineObjectDto.newBuilder()
+            .setKey("key")
+            .andAllOptionals()
+            .setData("data")
+            .setAnotherObject(anotherObjectDto)
+            .build();
+
+    final CustomerDto customerDto =
+        CustomerDto.newBuilder().setInlineObject(customerInlineObjectDto).andAllOptionals().build();
+
+    assertEquals(customerInlineObjectDto, customerDto.getInlineObject());
+    assertEquals(
+        Optional.of(anotherObjectDto), customerDto.getInlineObject().getAnotherObjectOptional());
+  }
+}
