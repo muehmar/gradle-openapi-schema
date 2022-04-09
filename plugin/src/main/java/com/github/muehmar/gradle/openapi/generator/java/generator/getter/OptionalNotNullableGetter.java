7062741eb5ee43966c4a38d76b0cd3352a8bb8a4
Martin Mühlemann <poelsi@gmail.com>
2022-04-09 21:19:45 +0200

    Add Jackson and Validation method for OptionalNotNullableGetter

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/getter/OptionalNotNullableGetter.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/getter/OptionalNotNullableGetter.java
@@ -1,14 +1,15 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.getter;
 
-import static com.github.muehmar.gradle.openapi.generator.java.generator.Filters.isJacksonJson;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.Filters.isValidationEnabled;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.ValidationGenerator.validationAnnotations;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.getter.CommonGetter.nullableGetterMethod;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.getter.CommonGetter.wrapNullableInOptionalGetterMethod;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.jackson.JacksonAnnotationGenerator.jsonIgnore;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.jackson.JacksonAnnotationGenerator.jsonIncludeNonNull;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.jackson.JacksonAnnotationGenerator.jsonProperty;
-import static io.github.muehmar.pojoextension.generator.impl.gen.Generators.newLine;
 
 import com.github.muehmar.gradle.openapi.generator.data.PojoMember;
+import com.github.muehmar.gradle.openapi.generator.java.generator.Filters;
 import com.github.muehmar.gradle.openapi.generator.java.generator.RefsGenerator;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.pojoextension.generator.Generator;
@@ -18,12 +19,24 @@ public class OptionalNotNullableGetter {
 
   public static Generator<PojoMember, PojoSettings> getter() {
     return Generator.<PojoMember, PojoSettings>emptyGen()
+        .append(standardGetter())
+        .append(jacksonOrValidationGetter())
+        .append(RefsGenerator.fieldRefs());
+  }
+
+  private static Generator<PojoMember, PojoSettings> standardGetter() {
+    return Generator.<PojoMember, PojoSettings>emptyGen()
         .append(jsonIgnore())
-        .append(wrapNullableInOptionalGetterMethod())
-        .appendConditionally(isJacksonJson(), newLine())
+        .append(wrapNullableInOptionalGetterMethod());
+  }
+
+  private static Generator<PojoMember, PojoSettings> jacksonOrValidationGetter() {
+    return Generator.<PojoMember, PojoSettings>emptyGen()
+        .appendNewLine()
         .append(jsonProperty())
         .append(jsonIncludeNonNull())
-        .appendConditionally(isJacksonJson(), nullableGetterMethod())
-        .append(RefsGenerator.fieldRefs());
+        .append(validationAnnotations())
+        .append(nullableGetterMethod())
+        .filter(Filters.<PojoMember>isJacksonJson().or(isValidationEnabled()));
   }
 }
--- plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/getter/OptionalNotNullableGetterTest.java
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/getter/OptionalNotNullableGetterTest.java
@@ -3,11 +3,15 @@ package com.github.muehmar.gradle.openapi.generator.java.generator.getter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.github.muehmar.gradle.openapi.generator.constraints.Constraints;
+import com.github.muehmar.gradle.openapi.generator.constraints.Pattern;
 import com.github.muehmar.gradle.openapi.generator.data.Name;
 import com.github.muehmar.gradle.openapi.generator.data.Necessity;
 import com.github.muehmar.gradle.openapi.generator.data.Nullability;
 import com.github.muehmar.gradle.openapi.generator.data.PojoMember;
+import com.github.muehmar.gradle.openapi.generator.java.JacksonRefs;
 import com.github.muehmar.gradle.openapi.generator.java.JavaRefs;
+import com.github.muehmar.gradle.openapi.generator.java.JavaValidationRefs;
 import com.github.muehmar.gradle.openapi.generator.java.type.JavaTypes;
 import com.github.muehmar.gradle.openapi.generator.settings.JsonSupport;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
@@ -32,8 +36,12 @@ class OptionalNotNullableGetterTest {
     final Writer writer =
         generator.generate(pojoMember, TestPojoSettings.defaultSettings(), Writer.createDefault());
 
+    assertEquals(5, writer.getRefs().size());
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_TIME_LOCAL_DATE::equals));
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_UTIL_OPTIONAL::equals));
+    assertTrue(writer.getRefs().exists(JacksonRefs.JSON_IGNORE::equals));
+    assertTrue(writer.getRefs().exists(JacksonRefs.JSON_PROPERTY::equals));
+    assertTrue(writer.getRefs().exists(JacksonRefs.JSON_INCLUDE::equals));
     assertEquals(
         "@JsonIgnore\n"
             + "public Optional<LocalDate> getBirthdate() {\n"
@@ -49,7 +57,41 @@ class OptionalNotNullableGetterTest {
   }
 
   @Test
-  void generator_when_disabledJackson_then_correctOutputAndRefs() {
+  void generator_when_disabledJacksonAndEnabledValidation_then_correctOutputAndRefs() {
+    final Generator<PojoMember, PojoSettings> generator = OptionalNotNullableGetter.getter();
+    final PojoMember pojoMember =
+        new PojoMember(
+            Name.of("birthdate"),
+            "Birthdate",
+            JavaTypes.LOCAL_DATE.withConstraints(
+                Constraints.ofPattern(Pattern.ofUnescapedString("DatePattern"))),
+            Necessity.OPTIONAL,
+            Nullability.NOT_NULLABLE);
+
+    final Writer writer =
+        generator.generate(
+            pojoMember,
+            TestPojoSettings.defaultSettings().withJsonSupport(JsonSupport.NONE),
+            Writer.createDefault());
+
+    assertEquals(3, writer.getRefs().size());
+    assertTrue(writer.getRefs().exists(JavaRefs.JAVA_TIME_LOCAL_DATE::equals));
+    assertTrue(writer.getRefs().exists(JavaRefs.JAVA_UTIL_OPTIONAL::equals));
+    assertTrue(writer.getRefs().exists(JavaValidationRefs.PATTERN::equals));
+    assertEquals(
+        "public Optional<LocalDate> getBirthdate() {\n"
+            + "  return Optional.ofNullable(birthdate);\n"
+            + "}\n"
+            + "\n"
+            + "@Pattern(regexp=\"DatePattern\")\n"
+            + "private LocalDate getBirthdateNullable() {\n"
+            + "  return birthdate;\n"
+            + "}",
+        writer.asString());
+  }
+
+  @Test
+  void generator_when_disabledJacksonAndValidation_then_correctOutputAndRefs() {
     final Generator<PojoMember, PojoSettings> generator = OptionalNotNullableGetter.getter();
     final PojoMember pojoMember =
         new PojoMember(
@@ -62,9 +104,12 @@ class OptionalNotNullableGetterTest {
     final Writer writer =
         generator.generate(
             pojoMember,
-            TestPojoSettings.defaultSettings().withJsonSupport(JsonSupport.NONE),
+            TestPojoSettings.defaultSettings()
+                .withJsonSupport(JsonSupport.NONE)
+                .withEnableConstraints(false),
             Writer.createDefault());
 
+    assertEquals(2, writer.getRefs().size());
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_TIME_LOCAL_DATE::equals));
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_UTIL_OPTIONAL::equals));
     assertEquals(
