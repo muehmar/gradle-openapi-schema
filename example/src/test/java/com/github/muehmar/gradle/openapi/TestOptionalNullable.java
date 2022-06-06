eb75e60f58ba3e29367640f606d545dbe4a1241d
Martin Mühlemann <poelsi@gmail.com>
2022-06-06 20:04:54 +0200

    Fix jackson order

--- example/src/test/java/com/github/muehmar/gradle/openapi/TestOptionalNullable.java
+++ example/src/test/java/com/github/muehmar/gradle/openapi/TestOptionalNullable.java
@@ -3,6 +3,7 @@ package com.github.muehmar.gradle.openapi;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import java.util.Set;
@@ -10,18 +11,24 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class TestOptionalNullable {
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  @BeforeAll
+  static void setupMapper() {
+    MAPPER.setConfig(
+        MAPPER.getSerializationConfig().with(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY));
+  }
 
   @Test
   void deserialize_when_onlyProp1Present_then_correctProps() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
-        mapper.readValue("{\"prop1\":\"Hello\",\"prop2\":null}", OptionalNullableDto.class);
+        MAPPER.readValue("{\"prop1\":\"Hello\",\"prop2\":null}", OptionalNullableDto.class);
 
     assertEquals("Hello", dto.getProp1());
     assertEquals(Optional.empty(), dto.getProp2());
@@ -31,10 +38,8 @@ class TestOptionalNullable {
 
   @Test
   void deserialize_when_prop2NotNull_then_correctProps() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
-        mapper.readValue("{\"prop1\":\"Hello\",\"prop2\":\"World\"}", OptionalNullableDto.class);
+        MAPPER.readValue("{\"prop1\":\"Hello\",\"prop2\":\"World\"}", OptionalNullableDto.class);
 
     assertEquals("Hello", dto.getProp1());
     assertEquals(Optional.of("World"), dto.getProp2());
@@ -44,10 +49,8 @@ class TestOptionalNullable {
 
   @Test
   void deserialize_when_prop3Present_then_correctProps() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
-        mapper.readValue(
+        MAPPER.readValue(
             "{\"prop1\":\"Hello\",\"prop2\":null,\"prop3\":\"World\"}", OptionalNullableDto.class);
 
     assertEquals("Hello", dto.getProp1());
@@ -58,10 +61,8 @@ class TestOptionalNullable {
 
   @Test
   void deserialize_when_prop4Null_then_correctProps() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
-        mapper.readValue(
+        MAPPER.readValue(
             "{\"prop1\":\"Hello\",\"prop2\":null,\"prop4\":null}", OptionalNullableDto.class);
 
     assertEquals("Hello", dto.getProp1());
@@ -72,10 +73,8 @@ class TestOptionalNullable {
 
   @Test
   void deserialize_when_prop4Present_then_correctProps() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
-        mapper.readValue(
+        MAPPER.readValue(
             "{\"prop1\":\"Hello\",\"prop2\":null,\"prop4\":\"World\"}", OptionalNullableDto.class);
 
     assertEquals("Hello", dto.getProp1());
@@ -86,46 +85,38 @@ class TestOptionalNullable {
 
   @Test
   void serialize_when_onlyProp1Present_then_correctSerialized() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto = new OptionalNullableDto("Hello", null, true, null, null, false);
 
-    final String output = mapper.writeValueAsString(dto);
+    final String output = MAPPER.writeValueAsString(dto);
 
     assertEquals("{\"prop1\":\"Hello\",\"prop2\":null}", output);
   }
 
   @Test
   void serialize_when_prop2Present_then_correctSerialized() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
         new OptionalNullableDto("Hello", "World", true, null, null, false);
 
-    final String output = mapper.writeValueAsString(dto);
+    final String output = MAPPER.writeValueAsString(dto);
 
     assertEquals("{\"prop1\":\"Hello\",\"prop2\":\"World\"}", output);
   }
 
   @Test
   void serialize_when_prop3Present_then_correctSerialized() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
         new OptionalNullableDto("Hello", null, true, "World", null, false);
 
-    final String output = mapper.writeValueAsString(dto);
+    final String output = MAPPER.writeValueAsString(dto);
 
     assertEquals("{\"prop1\":\"Hello\",\"prop2\":null,\"prop3\":\"World\"}", output);
   }
 
   @Test
   void serialize_when_prop4Null_then_correctSerialized() throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto = new OptionalNullableDto("Hello", null, true, null, null, true);
 
-    final String output = mapper.writeValueAsString(dto);
+    final String output = MAPPER.writeValueAsString(dto);
 
     assertEquals("{\"prop1\":\"Hello\",\"prop2\":null,\"prop4\":null}", output);
   }
@@ -134,12 +125,10 @@ class TestOptionalNullable {
   @ValueSource(booleans = {true, false})
   void serialize_when_prop4Present_then_correctSerialized(boolean isProp4Null)
       throws JsonProcessingException {
-    final ObjectMapper mapper = new ObjectMapper();
-
     final OptionalNullableDto dto =
         new OptionalNullableDto("Hello", null, true, null, "World", isProp4Null);
 
-    final String output = mapper.writeValueAsString(dto);
+    final String output = MAPPER.writeValueAsString(dto);
 
     assertEquals("{\"prop1\":\"Hello\",\"prop2\":null,\"prop4\":\"World\"}", output);
   }
