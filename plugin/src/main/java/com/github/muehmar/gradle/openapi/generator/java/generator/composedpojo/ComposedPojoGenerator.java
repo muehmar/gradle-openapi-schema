332955c6086c09b0c35711b03d89d6de88bd38ff
Martin Mühlemann <poelsi@gmail.com>
2022-12-26 08:59:43 +0100

    Add class skeleton
    #6
    #7

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/ComposedPojoGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/ComposedPojoGenerator.java
@@ -1,20 +1,43 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.composedpojo;
 
-import com.github.muehmar.gradle.openapi.generator.model.pojo.ComposedPojo;
+import static io.github.muehmar.codegenerator.java.ClassGen.Declaration.TOP_LEVEL;
+import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
+
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaDocGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.PackageGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaComposedPojo;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.java.ClassGenBuilder;
 import io.github.muehmar.codegenerator.writer.Writer;
 
-public class ComposedPojoGenerator implements Generator<ComposedPojo, PojoSettings> {
+public class ComposedPojoGenerator implements Generator<JavaComposedPojo, PojoSettings> {
 
-  private final Generator<ComposedPojo, PojoSettings> delegate;
+  private final Generator<JavaComposedPojo, PojoSettings> delegate;
 
   public ComposedPojoGenerator() {
-    this.delegate = Generator.emptyGen();
+    this.delegate =
+        ClassGenBuilder.<JavaComposedPojo, PojoSettings>create()
+            .enum_()
+            .declaration(TOP_LEVEL)
+            .packageGen(new PackageGenerator<>())
+            .javaDoc(
+                JavaDocGenerator.<PojoSettings>javaDoc()
+                    .contraMap(JavaComposedPojo::getDescription))
+            .modifiers(PUBLIC)
+            .className(enumPojo -> enumPojo.getName().asString())
+            .noSuperClass()
+            .noInterfaces()
+            .content(content())
+            .build();
+  }
+
+  private Generator<JavaComposedPojo, PojoSettings> content() {
+    return Generator.emptyGen();
   }
 
   @Override
-  public Writer generate(ComposedPojo data, PojoSettings settings, Writer writer) {
+  public Writer generate(JavaComposedPojo data, PojoSettings settings, Writer writer) {
     return delegate.generate(data, settings, writer);
   }
 }
--- plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/model/JavaPojos.java
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/model/JavaPojos.java
@@ -9,19 +9,27 @@ import ch.bluecare.commons.data.PList;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaArrayPojo;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaEnumPojo;
 import com.github.muehmar.gradle.openapi.generator.model.Name;
+import com.github.muehmar.gradle.openapi.generator.model.Pojo;
 import com.github.muehmar.gradle.openapi.generator.model.PojoName;
+import com.github.muehmar.gradle.openapi.generator.model.UnresolvedComposedPojo;
 import com.github.muehmar.gradle.openapi.generator.model.constraints.Constraints;
 import com.github.muehmar.gradle.openapi.generator.model.pojo.ArrayPojo;
+import com.github.muehmar.gradle.openapi.generator.model.pojo.ComposedPojo;
 import com.github.muehmar.gradle.openapi.generator.model.pojo.EnumPojo;
 import com.github.muehmar.gradle.openapi.generator.model.pojo.ObjectPojo;
 import com.github.muehmar.gradle.openapi.generator.model.type.NumericType;
 import com.github.muehmar.gradle.openapi.generator.settings.TypeMappings;
+import java.util.Optional;
 
 public class JavaPojos {
   private JavaPojos() {}
 
   public static JavaPojo allNecessityAndNullabilityVariants() {
-    return JavaPojo.wrap(
+    return JavaPojo.wrap(allNecessityAndNullabilityVariantsPojo(), TypeMappings.empty());
+  }
+
+  private static ObjectPojo allNecessityAndNullabilityVariantsPojo() {
+    final ObjectPojo allNecessityAndNullabilityVariantsPojo =
         ObjectPojo.of(
             PojoName.ofNameAndSuffix(Name.ofString("NecessityAndNullability"), "Dto"),
             "NecessityAndNullability",
@@ -29,8 +37,8 @@ public class JavaPojos {
                 requiredString(),
                 requiredNullableString(),
                 optionalString(),
-                optionalNullableString())),
-        TypeMappings.empty());
+                optionalNullableString()));
+    return allNecessityAndNullabilityVariantsPojo;
   }
 
   public static JavaPojo arrayPojo() {
@@ -50,5 +58,23 @@ public class JavaPojos {
             "Gender of person",
             PList.of("male", "female", "divers", "other"));
     return JavaEnumPojo.wrap(enumPojo);
+  }
+
+  public static JavaPojo composedPojo(ComposedPojo.CompositionType type) {
+    final Name typeName =
+        Name.ofString(type.name().toLowerCase().replace("_", "")).startUpperCase();
+    final UnresolvedComposedPojo unresolvedComposedPojo =
+        new UnresolvedComposedPojo(
+            PojoName.ofNameAndSuffix(typeName.prefix("Composed"), "Dto"),
+            "Composition Description",
+            UnresolvedComposedPojo.CompositionType.ONE_OF,
+            PList.empty(),
+            Optional.empty());
+    final PList<Pojo> pojos = PList.of(allNecessityAndNullabilityVariantsPojo());
+    final ComposedPojo composedPojo =
+        type.equals(ComposedPojo.CompositionType.ANY_OF)
+            ? ComposedPojo.resolvedAnyOf(pojos, unresolvedComposedPojo)
+            : ComposedPojo.resolvedOneOf(pojos, unresolvedComposedPojo);
+    return JavaPojo.wrap(composedPojo, TypeMappings.empty());
   }
 }
