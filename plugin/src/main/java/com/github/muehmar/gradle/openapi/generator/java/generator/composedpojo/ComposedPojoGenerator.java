7e90b949b9600138e471b87bda270d31e063cee5
Martin Mühlemann <poelsi@gmail.com>
2022-12-30 22:38:01 +0100

    Add member getters for serialisation

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/ComposedPojoGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/ComposedPojoGenerator.java
@@ -1,18 +1,23 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.composedpojo;
 
 import static io.github.muehmar.codegenerator.java.ClassGen.Declaration.TOP_LEVEL;
+import static io.github.muehmar.codegenerator.java.JavaModifier.PRIVATE;
 import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
 
+import com.github.muehmar.gradle.openapi.generator.java.OpenApiUtilRefs;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.NewFieldsGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.NewPojoConstructorGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.NormalBuilderGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaDocGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.generator.shared.PackageGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaComposedPojo;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
 import io.github.muehmar.codegenerator.java.ClassGenBuilder;
+import io.github.muehmar.codegenerator.java.MethodGen;
+import io.github.muehmar.codegenerator.java.MethodGenBuilder;
 import io.github.muehmar.codegenerator.writer.Writer;
 
 public class ComposedPojoGenerator implements Generator<JavaComposedPojo, PojoSettings> {
@@ -48,6 +53,54 @@ public class ComposedPojoGenerator implements Generator<JavaComposedPojo, PojoSe
         .appendNewLine()
         .append(NewPojoConstructorGenerator.generator(), JavaComposedPojo::wrapIntoJavaObjectPojo)
         .appendNewLine()
-        .append(new NormalBuilderGenerator(), JavaComposedPojo::wrapIntoJavaObjectPojo);
+        .append(new NormalBuilderGenerator(), JavaComposedPojo::wrapIntoJavaObjectPojo)
+        .appendList(memberGetter().prependNewLine(), JavaComposedPojo::getMembers);
+  }
+
+  private Generator<JavaPojoMember, PojoSettings> memberGetter() {
+    final MethodGen<JavaPojoMember, PojoSettings> method =
+        MethodGenBuilder.<JavaPojoMember, PojoSettings>create()
+            .modifiers(PRIVATE)
+            .noGenericTypes()
+            .returnType(member -> member.getJavaType().getFullClassName().asString())
+            .methodName(member -> member.getGetterName().asString())
+            .noArguments()
+            .content(memberGetterContent())
+            .build();
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(JacksonAnnotationGenerator.jsonProperty())
+        .append(JacksonAnnotationGenerator.jsonIncludeNonNull())
+        .append(method)
+        .filter((member, settings) -> settings.isJacksonJson());
+  }
+
+  private Generator<JavaPojoMember, PojoSettings> memberGetterContent() {
+    final Generator<JavaPojoMember, PojoSettings> requiredOrOptionalMember =
+        Generator.<JavaPojoMember, PojoSettings>emptyGen()
+            .append((member, settings, writer) -> writer.println("return %s;", member.getName()))
+            .filter(
+                member -> member.isRequiredAndNotNullable() || member.isOptionalAndNotNullable());
+
+    final Generator<JavaPojoMember, PojoSettings> requiredNullableMember =
+        Generator.<JavaPojoMember, PojoSettings>emptyGen()
+            .append(
+                (member, settings, writer) ->
+                    writer.println(
+                        "return %s ? new JacksonNullContainer<>(%s) : null;",
+                        member.getIsPresentFlagName(), member.getName()))
+            .append(w -> w.ref(OpenApiUtilRefs.JACKSON_NULL_CONTAINER))
+            .filter(JavaPojoMember::isRequiredAndNullable);
+
+    final Generator<JavaPojoMember, PojoSettings> optionalNullableMember =
+        Generator.<JavaPojoMember, PojoSettings>emptyGen()
+            .append(
+                (member, settings, writer) ->
+                    writer.println(
+                        "return %s ? new JacksonNullContainer<>(%s) : %s;",
+                        member.getIsNullFlagName(), member.getName(), member.getName()))
+            .append(w -> w.ref(OpenApiUtilRefs.JACKSON_NULL_CONTAINER))
+            .filter(JavaPojoMember::isOptionalAndNullable);
+
+    return requiredOrOptionalMember.append(requiredNullableMember).append(optionalNullableMember);
   }
 }
