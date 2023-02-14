8524044fb37816600823d325488de197a81e57e1
Martin Mühlemann <poelsi@gmail.com>
2023-02-15 00:31:34 +0100

    Add DiscriminatorValidationMethodGenerator

---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/DiscriminatorValidationMethodGenerator.java
@@ -0,0 +1,81 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.composedpojo;
+
+import static io.github.muehmar.codegenerator.Generator.constant;
+import static io.github.muehmar.codegenerator.Generator.ofWriterFunction;
+
+import ch.bluecare.commons.data.PList;
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.ValidationGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojo;
+import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaComposedPojo;
+import com.github.muehmar.gradle.openapi.generator.model.Discriminator;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.java.MethodGen;
+import io.github.muehmar.codegenerator.java.MethodGenBuilder;
+import java.util.Optional;
+import lombok.Value;
+
+public class DiscriminatorValidationMethodGenerator {
+  private DiscriminatorValidationMethodGenerator() {}
+
+  public static Generator<JavaComposedPojo, PojoSettings> generator() {
+    final Generator<PojoAndDiscriminator, PojoSettings> annotation =
+        ValidationGenerator.assertTrue(
+            pojo -> "Not valid against the schema described by the discriminator");
+    final MethodGen<PojoAndDiscriminator, PojoSettings> method =
+        MethodGenBuilder.<PojoAndDiscriminator, PojoSettings>create()
+            .modifiers((pojo, settings) -> settings.getRawGetter().getModifier().asJavaModifiers())
+            .noGenericTypes()
+            .returnType("boolean")
+            .methodName("isValidAgainstTheCorrectSchema")
+            .noArguments()
+            .content(methodContent())
+            .build();
+
+    return Generator.<JavaComposedPojo, PojoSettings>emptyGen()
+        .appendOptional(annotation.append(method), PojoAndDiscriminator::fromPojo);
+  }
+
+  private static Generator<PojoAndDiscriminator, PojoSettings> methodContent() {
+    return Generator.<PojoAndDiscriminator, PojoSettings>of(
+            (p, s, w) -> w.println("if (%s == null) {", p.getDiscriminator().getPropertyName()))
+        .append(ofWriterFunction(w -> w.tab(1).println("return false;")))
+        .append(constant("}"))
+        .append((p, s, w) -> w.println("switch(%s) {", p.getDiscriminator().getPropertyName()))
+        .appendList(gen().indent(1), PojoAndDiscriminator::getPojos)
+        .append(constant("}"))
+        .append(constant("return false;"));
+  }
+
+  private static Generator<SinglePojoAndDiscriminator, PojoSettings> gen() {
+    return Generator.of(
+        (p, s, w) ->
+            w.println(
+                "case \"%s\": return isValidAgainst%s();",
+                p.getDiscriminatorValue(), p.getPojo().getName().getName()));
+  }
+
+  @Value
+  private static class PojoAndDiscriminator {
+    JavaComposedPojo pojo;
+    Discriminator discriminator;
+
+    static Optional<PojoAndDiscriminator> fromPojo(JavaComposedPojo pojo) {
+      return pojo.getDiscriminator().map(d -> new PojoAndDiscriminator(pojo, d));
+    }
+
+    PList<SinglePojoAndDiscriminator> getPojos() {
+      return pojo.getJavaPojos().map(pojo -> new SinglePojoAndDiscriminator(pojo, discriminator));
+    }
+  }
+
+  @Value
+  private static class SinglePojoAndDiscriminator {
+    JavaPojo pojo;
+    Discriminator discriminator;
+
+    String getDiscriminatorValue() {
+      return discriminator.getValueForPojoName(pojo.getName());
+    }
+  }
+}
---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/composedpojo/DiscriminatorValidationMethodGeneratorTest.java
@@ -0,0 +1,66 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.composedpojo;
+
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import au.com.origin.snapshots.junit5.SnapshotExtension;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojos;
+import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaComposedPojo;
+import com.github.muehmar.gradle.openapi.generator.model.pojo.ComposedPojo;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import com.github.muehmar.gradle.openapi.generator.settings.TestPojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.writer.Writer;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+@ExtendWith(SnapshotExtension.class)
+class DiscriminatorValidationMethodGeneratorTest {
+  private Expect expect;
+
+  @ParameterizedTest
+  @EnumSource(ComposedPojo.CompositionType.class)
+  @SnapshotName("NoDiscriminator")
+  void generate_when_calledWithoutDiscriminator_then_noContent(ComposedPojo.CompositionType type) {
+    final Generator<JavaComposedPojo, PojoSettings> generator =
+        DiscriminatorValidationMethodGenerator.generator();
+    final Writer writer =
+        generator.generate(
+            JavaPojos.composedPojo(type),
+            TestPojoSettings.defaultSettings(),
+            Writer.createDefault());
+
+    expect.scenario(type.name()).toMatchSnapshot(writer.asString());
+  }
+
+  @ParameterizedTest
+  @EnumSource(ComposedPojo.CompositionType.class)
+  @SnapshotName("DiscriminatorWithoutMapping")
+  void generate_when_calledWithDiscriminator_then_noContent(ComposedPojo.CompositionType type) {
+    final Generator<JavaComposedPojo, PojoSettings> generator =
+        DiscriminatorValidationMethodGenerator.generator();
+    final Writer writer =
+        generator.generate(
+            JavaPojos.composedPojoWithDiscriminator(type),
+            TestPojoSettings.defaultSettings(),
+            Writer.createDefault());
+
+    expect.scenario(type.name()).toMatchSnapshot(writer.asString());
+  }
+
+  @ParameterizedTest
+  @EnumSource(ComposedPojo.CompositionType.class)
+  @SnapshotName("DiscriminatorWithMapping")
+  void generate_when_calledWithDiscriminatorAndMapping_then_noContent(
+      ComposedPojo.CompositionType type) {
+    final Generator<JavaComposedPojo, PojoSettings> generator =
+        DiscriminatorValidationMethodGenerator.generator();
+    final Writer writer =
+        generator.generate(
+            JavaPojos.composedPojoWithDiscriminatorMapping(type),
+            TestPojoSettings.defaultSettings(),
+            Writer.createDefault());
+
+    expect.scenario(type.name()).toMatchSnapshot(writer.asString());
+  }
+}
