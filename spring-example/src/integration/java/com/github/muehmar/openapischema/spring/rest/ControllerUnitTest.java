4cc833c35ac048e6e448670a3452f878221d3ce8
Martin Mühlemann <poelsi@gmail.com>
2023-03-13 09:12:18 +0100

    Add AnyOf spring example

--- spring-example/src/integration/java/com/github/muehmar/openapischema/spring/rest/ControllerUnitTest.java
+++ spring-example/src/integration/java/com/github/muehmar/openapischema/spring/rest/ControllerUnitTest.java
@@ -1,5 +1,7 @@
 package com.github.muehmar.openapischema.spring.rest;
 
+import com.github.muehmar.openapischema.spring.rest.anyof.AnyOfController;
+import com.github.muehmar.openapischema.spring.rest.anyof.AnyOfInterface;
 import com.github.muehmar.openapischema.spring.rest.oneof.OneOfController;
 import com.github.muehmar.openapischema.spring.rest.oneof.OneOfInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 public class ControllerUnitTest {
 
   @MockBean private OneOfInterface oneOfInterface;
+  @MockBean private AnyOfInterface anyOfInterface;
 
   @Autowired private OneOfController oneOfController;
+  @Autowired private AnyOfController anyOfController;
 }
