1fe6a8a370300487aac2788badd4bf18a0083c70
Martin Mühlemann <poelsi@gmail.com>
2023-04-01 12:55:45 +0200

    Add allOf example test

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/allof/AllOfArrayTest.java
@@ -0,0 +1,25 @@
+package com.github.muehmar.gradle.openapi.allof;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import OpenApiSchema.example.api.allof.model.ReportDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+class AllOfArrayTest {
+  private static final ObjectMapper MAPPER = new ObjectMapper();
+
+  @Test
+  void deserialize_when_executed_then_arrayDeserialized() throws JsonProcessingException {
+    final ReportDto report =
+        MAPPER.readValue(
+            "{\"invoices\":[{\"orderNumber\":1234,\"title\":\"Hello World!\",\"paid\":false}]}",
+            ReportDto.class);
+
+    assertEquals(1, report.getInvoices().size());
+    assertEquals(1234, report.getInvoices().get(0).getOrderNumber());
+    assertEquals("Hello World!", report.getInvoices().get(0).getTitle());
+    assertEquals(false, report.getInvoices().get(0).getPaid());
+  }
+}
