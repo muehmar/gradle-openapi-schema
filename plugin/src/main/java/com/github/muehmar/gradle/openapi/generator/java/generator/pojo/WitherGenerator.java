2258a71d4c61477ace137125a613523811b260e2
Martin Mühlemann <poelsi@gmail.com>
2023-04-06 14:29:46 +0200

    Replace the wither generation with a generator
    #70

---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/WitherGenerator.java
@@ -0,0 +1,91 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo;
+
+import static io.github.muehmar.codegenerator.Generator.newLine;
+import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
+
+import ch.bluecare.commons.data.PList;
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaDocGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojo;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.java.MethodGen;
+import io.github.muehmar.codegenerator.java.MethodGenBuilder;
+
+public class WitherGenerator {
+  private WitherGenerator() {}
+
+  public static Generator<JavaPojo, PojoSettings> generator() {
+    return Generator.<JavaPojo, PojoSettings>emptyGen()
+        .appendList(method(), WitherMethod::fromPojo, newLine());
+  }
+
+  private static Generator<WitherMethod, PojoSettings> method() {
+    final MethodGen<WitherMethod, PojoSettings> method =
+        MethodGenBuilder.<WitherMethod, PojoSettings>create()
+            .modifiers(PUBLIC)
+            .noGenericTypes()
+            .returnType(WitherMethod::className)
+            .methodName(WitherMethod::witherName)
+            .arguments(WitherMethod::argument)
+            .content(methodContent())
+            .build();
+    return JavaDocGenerator.<PojoSettings>javaDoc()
+        .<WitherMethod>contraMap(WitherMethod::javaDocString)
+        .append(method);
+  }
+
+  private static Generator<WitherMethod, PojoSettings> methodContent() {
+    return Generator.<WitherMethod, PojoSettings>emptyGen()
+        .append((wm, s, w) -> w.println("return %s;", wm.constructorCall()));
+  }
+
+  private abstract static class WitherMethod {
+    protected final JavaPojo pojo;
+    protected final JavaPojoMember pojoMember;
+
+    public static PList<WitherMethod> fromPojo(JavaPojo pojo) {
+      return pojo.getMembersOrEmpty().map(member -> new NormalWitherMethod(pojo, member));
+    }
+
+    public String javaDocString() {
+      return pojoMember.getDescription();
+    }
+
+    public WitherMethod(JavaPojo pojo, JavaPojoMember pojoMember) {
+      this.pojo = pojo;
+      this.pojoMember = pojoMember;
+    }
+
+    String className() {
+      return pojo.getClassName().asString();
+    }
+
+    String witherName() {
+      return pojoMember.getWitherName().asString();
+    }
+
+    abstract PList<String> argument();
+
+    abstract String constructorCall();
+  }
+
+  private static class NormalWitherMethod extends WitherMethod {
+    public NormalWitherMethod(JavaPojo pojo, JavaPojoMember pojoMember) {
+      super(pojo, pojoMember);
+    }
+
+    PList<String> argument() {
+      return PList.single(
+          String.format(
+              "%s %s", pojoMember.getJavaType().getFullClassName(), pojoMember.getName()));
+    }
+
+    String constructorCall() {
+      return String.format(
+          "new %s(%s)",
+          pojo.getClassName(),
+          pojo.getMembersOrEmpty().flatMap(JavaPojoMember::createFieldNames).mkString(", "));
+    }
+  }
+}
---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/WitherGeneratorTest.java
@@ -0,0 +1,33 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import au.com.origin.snapshots.junit5.SnapshotExtension;
+import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojo;
+import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaPojos;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import com.github.muehmar.gradle.openapi.generator.settings.TestPojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.writer.Writer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SnapshotExtension.class)
+class WitherGeneratorTest {
+  private Expect expect;
+
+  @Test
+  @SnapshotName("allNullabilityAndNecessityVariants")
+  void generate_when_calledWithNullabilityAndNecessityVariants_then_correctOutput() {
+    final Generator<JavaPojo, PojoSettings> generator = WitherGenerator.generator();
+    final Writer writer =
+        generator.generate(
+            JavaPojos.allNecessityAndNullabilityVariants(),
+            TestPojoSettings.defaultSettings(),
+            Writer.createDefault());
+
+    expect.toMatchSnapshot(writer.asString());
+  }
+}
