9f4ca85889a5c1c4c0db369bb066da3aab0af625
Martin Mühlemann <poelsi@gmail.com>
2023-04-06 08:44:53 +0200

    Refactor optional and required member builder

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/safebuilder/OptionalMemberBuilderGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/safebuilder/OptionalMemberBuilderGenerator.java
@@ -1,95 +1,85 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.shared.safebuilder;
 
-import static io.github.muehmar.codegenerator.Generator.constant;
-import static io.github.muehmar.codegenerator.java.JavaModifier.FINAL;
-import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
-import static io.github.muehmar.codegenerator.java.JavaModifier.STATIC;
-
 import ch.bluecare.commons.data.PList;
 import com.github.muehmar.gradle.openapi.generator.java.JavaRefs;
 import com.github.muehmar.gradle.openapi.generator.java.OpenApiUtilRefs;
-import com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaDocGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaObjectPojo;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
-import io.github.muehmar.codegenerator.java.ClassGenBuilder;
-import io.github.muehmar.codegenerator.java.MethodGenBuilder;
+import io.github.muehmar.codegenerator.writer.Writer;
 import lombok.Value;
 
 class OptionalMemberBuilderGenerator {
+
+  private static final SingleMemberBuilderGenerator.Setter<OptionalMember> NORMAL_SETTER =
+      new SingleMemberBuilderGenerator.Setter<OptionalMember>() {
+
+        @Override
+        public boolean includeInBuilder(OptionalMember member) {
+          return true;
+        }
+
+        @Override
+        public String argumentFormat() {
+          return "%s %s";
+        }
+
+        @Override
+        public Writer addRefs(Writer writer) {
+          return writer;
+        }
+      };
+  private static final SingleMemberBuilderGenerator.Setter<OptionalMember> OPTIONAL_SETTER =
+      new SingleMemberBuilderGenerator.Setter<OptionalMember>() {
+
+        @Override
+        public boolean includeInBuilder(OptionalMember member) {
+          return member.isNotNullable();
+        }
+
+        @Override
+        public String argumentFormat() {
+          return "Optional<%s> %s";
+        }
+
+        @Override
+        public Writer addRefs(Writer writer) {
+          return writer.ref(JavaRefs.JAVA_UTIL_OPTIONAL);
+        }
+      };
+  private static final SingleMemberBuilderGenerator.Setter<OptionalMember> TRISTATE_SETTER =
+      new SingleMemberBuilderGenerator.Setter<OptionalMember>() {
+
+        @Override
+        public boolean includeInBuilder(OptionalMember member) {
+          return member.isNullable();
+        }
+
+        @Override
+        public String argumentFormat() {
+          return "Tristate<%s> %s";
+        }
+
+        @Override
+        public Writer addRefs(Writer writer) {
+          return writer.ref(OpenApiUtilRefs.TRISTATE);
+        }
+      };
+
   private OptionalMemberBuilderGenerator() {}
 
   public static Generator<JavaObjectPojo, PojoSettings> generator() {
+    final PList<SingleMemberBuilderGenerator.Setter<OptionalMember>> setters =
+        PList.of(NORMAL_SETTER, OPTIONAL_SETTER, TRISTATE_SETTER);
+    final Generator<OptionalMember, PojoSettings> singelMemberGenerator =
+        SingleMemberBuilderGenerator.generator(setters);
     return Generator.<JavaObjectPojo, PojoSettings>emptyGen()
-        .appendList(optionalBuilder(), OptionalMember::fromObjectPojo, Generator.newLine());
-  }
-
-  private static Generator<OptionalMember, PojoSettings> optionalBuilder() {
-    return ClassGenBuilder.<OptionalMember, PojoSettings>create()
-        .clazz()
-        .nested()
-        .packageGen(Generator.emptyGen())
-        .noJavaDoc()
-        .noAnnotations()
-        .modifiers(PUBLIC, STATIC, FINAL)
-        .className(OptionalMember::builderClassName)
-        .noSuperClass()
-        .noInterfaces()
-        .content(optionalBuilderContent())
-        .build();
-  }
-
-  private static Generator<OptionalMember, PojoSettings> optionalBuilderContent() {
-    final Generator<OptionalMember, PojoSettings> normalSetter = optionalBuilderSetter("%s %s");
-    final Generator<OptionalMember, PojoSettings> optionalSetter =
-        optionalBuilderSetter("Optional<%s> %s").append(w -> w.ref(JavaRefs.JAVA_UTIL_OPTIONAL));
-    final Generator<OptionalMember, PojoSettings> tristateSetter =
-        optionalBuilderSetter("Tristate<%s> %s").append(w -> w.ref(OpenApiUtilRefs.TRISTATE));
-
-    return Generator.<OptionalMember, PojoSettings>emptyGen()
-        .append(constant("private final Builder builder;"))
-        .appendNewLine()
-        .append((m, s, w) -> w.println("private OptBuilder%d(Builder builder) {", m.idx))
-        .append(constant("this.builder = builder;"), 1)
-        .append(constant("}"))
-        .appendNewLine()
-        .append(normalSetter)
-        .appendSingleBlankLine()
-        .append(optionalSetter.filter(OptionalMember::isNotNullable))
-        .append(tristateSetter.filter(OptionalMember::isNullable));
-  }
-
-  private static Generator<OptionalMember, PojoSettings> optionalBuilderSetter(
-      String argumentFormat) {
-    final Generator<OptionalMember, PojoSettings> method =
-        MethodGenBuilder.<OptionalMember, PojoSettings>create()
-            .modifiers(PUBLIC)
-            .noGenericTypes()
-            .returnType(OptionalMember::nextBuilderClassName)
-            .methodName(
-                (m, s) -> m.member.prefixedMethodName(s.getBuilderMethodPrefix()).asString())
-            .singleArgument(
-                m ->
-                    String.format(
-                        argumentFormat,
-                        m.member.getJavaType().getFullClassName().asString(),
-                        m.member.getName()))
-            .content(
-                (m, s, w) ->
-                    w.println(
-                        "return new %s(builder.%s(%s));",
-                        m.nextBuilderClassName(),
-                        m.member.prefixedMethodName(s.getBuilderMethodPrefix()),
-                        m.member.getName()))
-            .build();
-    return JavaDocGenerator.<PojoSettings>javaDoc()
-        .<OptionalMember>contraMap(m -> m.member.getDescription())
-        .append(method);
+        .appendList(singelMemberGenerator, OptionalMember::fromObjectPojo, Generator.newLine());
   }
 
   @Value
-  private static class OptionalMember {
+  private static class OptionalMember implements SingleMemberBuilderGenerator.Member {
     JavaPojoMember member;
     int idx;
 
@@ -100,11 +90,11 @@ class OptionalMemberBuilderGenerator {
           .map(p -> new OptionalMember(p.first(), p.second()));
     }
 
-    private String builderClassName() {
+    public String builderClassName() {
       return String.format("OptBuilder%d", idx);
     }
 
-    private String nextBuilderClassName() {
+    public String nextBuilderClassName() {
       return String.format("OptBuilder%d", idx + 1);
     }
 
--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/safebuilder/RequiredMemberBuilderGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/safebuilder/RequiredMemberBuilderGenerator.java
@@ -1,90 +1,66 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.shared.safebuilder;
 
-import static io.github.muehmar.codegenerator.Generator.constant;
-import static io.github.muehmar.codegenerator.java.JavaModifier.FINAL;
-import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
-import static io.github.muehmar.codegenerator.java.JavaModifier.STATIC;
-
 import ch.bluecare.commons.data.PList;
 import com.github.muehmar.gradle.openapi.generator.java.JavaRefs;
-import com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaDocGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaObjectPojo;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
-import io.github.muehmar.codegenerator.java.ClassGenBuilder;
-import io.github.muehmar.codegenerator.java.MethodGenBuilder;
+import io.github.muehmar.codegenerator.writer.Writer;
 import lombok.Value;
 
 class RequiredMemberBuilderGenerator {
+
+  private static final SingleMemberBuilderGenerator.Setter<RequiredMember> NORMAL_SETTER =
+      new SingleMemberBuilderGenerator.Setter<RequiredMember>() {
+
+        @Override
+        public boolean includeInBuilder(RequiredMember member) {
+          return true;
+        }
+
+        @Override
+        public String argumentFormat() {
+          return "%s %s";
+        }
+
+        @Override
+        public Writer addRefs(Writer writer) {
+          return writer;
+        }
+      };
+  private static final SingleMemberBuilderGenerator.Setter<RequiredMember> OPTIONAL_SETTER =
+      new SingleMemberBuilderGenerator.Setter<RequiredMember>() {
+
+        @Override
+        public boolean includeInBuilder(RequiredMember member) {
+          return member.isNullable();
+        }
+
+        @Override
+        public String argumentFormat() {
+          return "Optional<%s> %s";
+        }
+
+        @Override
+        public Writer addRefs(Writer writer) {
+          return writer.ref(JavaRefs.JAVA_UTIL_OPTIONAL);
+        }
+      };
+
   private RequiredMemberBuilderGenerator() {}
 
   public static Generator<JavaObjectPojo, PojoSettings> generator() {
+    final PList<SingleMemberBuilderGenerator.Setter<RequiredMember>> setters =
+        PList.of(NORMAL_SETTER, OPTIONAL_SETTER);
+    final Generator<RequiredMember, PojoSettings> singleMemberGenerator =
+        SingleMemberBuilderGenerator.generator(setters);
     return Generator.<JavaObjectPojo, PojoSettings>emptyGen()
-        .appendList(requiredBuilder(), RequiredMember::fromObjectPojo, Generator.newLine());
-  }
-
-  private static Generator<RequiredMember, PojoSettings> requiredBuilder() {
-    return ClassGenBuilder.<RequiredMember, PojoSettings>create()
-        .clazz()
-        .nested()
-        .packageGen(Generator.emptyGen())
-        .noJavaDoc()
-        .noAnnotations()
-        .modifiers(STATIC, FINAL, PUBLIC)
-        .className(RequiredMember::builderClassName)
-        .noSuperClass()
-        .noInterfaces()
-        .content(requiredBuilderContent())
-        .build();
-  }
-
-  private static Generator<RequiredMember, PojoSettings> requiredBuilderContent() {
-    final Generator<RequiredMember, PojoSettings> normalSetter = requiredBuilderSetter("%s %s");
-    final Generator<RequiredMember, PojoSettings> nullableSetter =
-        requiredBuilderSetter("Optional<%s> %s").append(w -> w.ref(JavaRefs.JAVA_UTIL_OPTIONAL));
-    return Generator.<RequiredMember, PojoSettings>emptyGen()
-        .append(constant("private final Builder builder;"))
-        .appendNewLine()
-        .append((m, s, w) -> w.println("private Builder%d(Builder builder) {", m.idx))
-        .append(constant("this.builder = builder;"), 1)
-        .append(constant("}"))
-        .appendNewLine()
-        .append(normalSetter)
-        .appendSingleBlankLine()
-        .append(nullableSetter.filter(RequiredMember::isNullable));
-  }
-
-  private static Generator<RequiredMember, PojoSettings> requiredBuilderSetter(
-      String argumentFormat) {
-    final Generator<RequiredMember, PojoSettings> method =
-        MethodGenBuilder.<RequiredMember, PojoSettings>create()
-            .modifiers(PUBLIC)
-            .noGenericTypes()
-            .returnType(RequiredMember::nextBuilderClassName)
-            .methodName(
-                (m, s) -> m.member.prefixedMethodName(s.getBuilderMethodPrefix()).asString())
-            .singleArgument(
-                m ->
-                    String.format(
-                        argumentFormat,
-                        m.member.getJavaType().getFullClassName().asString(),
-                        m.member.getName()))
-            .content(
-                (m, s, w) ->
-                    w.println(
-                        "return new %s(builder.%s(%s));",
-                        m.nextBuilderClassName(),
-                        m.member.prefixedMethodName(s.getBuilderMethodPrefix()),
-                        m.member.getName()))
-            .build();
-    return JavaDocGenerator.<PojoSettings>javaDoc()
-        .<RequiredMember>contraMap(m -> m.member.getDescription())
-        .append(method);
+        .appendList(singleMemberGenerator, RequiredMember::fromObjectPojo, Generator.newLine());
   }
 
   @Value
-  private static class RequiredMember {
+  private static class RequiredMember implements SingleMemberBuilderGenerator.Member {
     JavaPojoMember member;
     int idx;
 
@@ -95,11 +71,11 @@ class RequiredMemberBuilderGenerator {
           .map(p -> new RequiredMember(p.first(), p.second()));
     }
 
-    private String builderClassName() {
+    public String builderClassName() {
       return String.format("Builder%d", idx);
     }
 
-    private String nextBuilderClassName() {
+    public String nextBuilderClassName() {
       return String.format("Builder%d", idx + 1);
     }
 
