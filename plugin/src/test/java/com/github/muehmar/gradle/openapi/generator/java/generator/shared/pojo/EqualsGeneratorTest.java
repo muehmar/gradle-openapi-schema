f69366a8cae83892e9c15a9921c4464315e9bdaf
Martin Mühlemann <poelsi@gmail.com>
2023-04-20 23:21:09 +0200

    Fix equals method in case of property is named other
    #71

--- plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/pojo/EqualsGeneratorTest.java
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/shared/pojo/EqualsGeneratorTest.java
@@ -3,6 +3,9 @@ package com.github.muehmar.gradle.openapi.generator.java.generator.shared.pojo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import au.com.origin.snapshots.junit5.SnapshotExtension;
 import ch.bluecare.commons.data.PList;
 import com.github.muehmar.gradle.openapi.generator.java.JavaRefs;
 import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojo;
@@ -12,9 +15,14 @@ import com.github.muehmar.gradle.openapi.generator.settings.TestPojoSettings;
 import io.github.muehmar.codegenerator.Generator;
 import io.github.muehmar.codegenerator.writer.Writer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(SnapshotExtension.class)
 class EqualsGeneratorTest {
+  private Expect expect;
+
   @Test
+  @SnapshotName("allNecessityAndNullabilityVariants")
   void generate_when_allNecessityAndNullabilityVariants_then_correctEqualsMethod() {
     final Generator<JavaPojo, PojoSettings> generator = EqualsGenerator.equalsMethod();
 
@@ -26,23 +34,11 @@ class EqualsGeneratorTest {
 
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_UTIL_OBJECTS::equals));
 
-    assertEquals(
-        "@Override\n"
-            + "public boolean equals(Object obj) {\n"
-            + "  if (this == obj) return true;\n"
-            + "  if (obj == null || this.getClass() != obj.getClass()) return false;\n"
-            + "  final NecessityAndNullabilityDto other = (NecessityAndNullabilityDto) obj;\n"
-            + "  return Objects.equals(requiredStringVal, other.requiredStringVal)\n"
-            + "      && Objects.equals(requiredNullableStringVal, other.requiredNullableStringVal)\n"
-            + "      && Objects.equals(isRequiredNullableStringValPresent, other.isRequiredNullableStringValPresent)\n"
-            + "      && Objects.equals(optionalStringVal, other.optionalStringVal)\n"
-            + "      && Objects.equals(optionalNullableStringVal, other.optionalNullableStringVal)\n"
-            + "      && Objects.equals(isOptionalNullableStringValNull, other.isOptionalNullableStringValNull);\n"
-            + "}",
-        writer.asString());
+    expect.toMatchSnapshot(writer.asString());
   }
 
   @Test
+  @SnapshotName("arrayPojo")
   void generate_when_arrayPojo_then_correctEqualsMethod() {
     final Generator<JavaPojo, PojoSettings> generator = EqualsGenerator.equalsMethod();
 
@@ -52,15 +48,7 @@ class EqualsGeneratorTest {
 
     assertTrue(writer.getRefs().exists(JavaRefs.JAVA_UTIL_OBJECTS::equals));
 
-    assertEquals(
-        "@Override\n"
-            + "public boolean equals(Object obj) {\n"
-            + "  if (this == obj) return true;\n"
-            + "  if (obj == null || this.getClass() != obj.getClass()) return false;\n"
-            + "  final PosologyDto other = (PosologyDto) obj;\n"
-            + "  return Objects.equals(value, other.value);\n"
-            + "}",
-        writer.asString());
+    expect.toMatchSnapshot(writer.asString());
   }
 
   @Test
