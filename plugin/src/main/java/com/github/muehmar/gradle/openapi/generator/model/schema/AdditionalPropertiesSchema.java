7f791fc51458e0360628589b0999bfa4f2b84a8d
Martin Mühlemann <poelsi@gmail.com>
2023-05-10 08:12:02 +0200

    Wrap AdditionalProperties object
    #76

---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/model/schema/AdditionalPropertiesSchema.java
@@ -0,0 +1,49 @@
+package com.github.muehmar.gradle.openapi.generator.model.schema;
+
+import com.github.muehmar.gradle.openapi.generator.mapper.MemberSchemaMapResult;
+import com.github.muehmar.gradle.openapi.generator.model.Name;
+import com.github.muehmar.gradle.openapi.generator.model.PojoName;
+import com.github.muehmar.gradle.openapi.generator.model.Type;
+import com.github.muehmar.gradle.openapi.generator.model.type.NoType;
+import io.swagger.v3.oas.models.media.Schema;
+import java.util.Optional;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/** Wraps the additionalProperties object which can be either a boolean or a schema. */
+@EqualsAndHashCode
+@ToString
+class AdditionalPropertiesSchema {
+  private final boolean allowed;
+  private final Optional<OpenApiSchema> schema;
+
+  private AdditionalPropertiesSchema(boolean allowed, Optional<OpenApiSchema> schema) {
+    this.allowed = allowed;
+    this.schema = schema;
+  }
+
+  public static AdditionalPropertiesSchema wrapNullable(Object object) {
+    if (object == null || Boolean.TRUE.equals(object)) {
+      return new AdditionalPropertiesSchema(true, Optional.empty());
+    } else if (object instanceof Schema) {
+      final OpenApiSchema openApiSchema = OpenApiSchema.wrapSchema((Schema<?>) object);
+      return new AdditionalPropertiesSchema(true, Optional.of(openApiSchema));
+    } else {
+      return new AdditionalPropertiesSchema(false, Optional.empty());
+    }
+  }
+
+  public boolean isAllowed() {
+    return allowed;
+  }
+
+  public Type getAdditionalPropertiesType(PojoName pojoName) {
+    return getAdditionalPropertiesMapResult(pojoName).getType();
+  }
+
+  public MemberSchemaMapResult getAdditionalPropertiesMapResult(PojoName pojoName) {
+    return schema
+        .map(s -> s.mapToMemberType(pojoName, Name.ofString("Properties")))
+        .orElse(MemberSchemaMapResult.ofType(NoType.create()));
+  }
+}
---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/model/schema/AdditionalPropertiesSchemaTest.java
@@ -0,0 +1,61 @@
+package com.github.muehmar.gradle.openapi.generator.model.schema;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.github.muehmar.gradle.openapi.generator.model.PojoName;
+import com.github.muehmar.gradle.openapi.generator.model.Type;
+import com.github.muehmar.gradle.openapi.generator.model.type.NoType;
+import com.github.muehmar.gradle.openapi.generator.model.type.ObjectType;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AdditionalPropertiesSchemaTest {
+
+  @ParameterizedTest
+  @NullSource
+  @ValueSource(booleans = true)
+  void wrapNullable_when_nullOrTrue_then_additionalPropertiesAllowedAndAnyType(
+      Object additionalProperties) {
+    final AdditionalPropertiesSchema additionalPropertiesSchema =
+        AdditionalPropertiesSchema.wrapNullable(additionalProperties);
+
+    final Type additionalPropertiesType =
+        additionalPropertiesSchema.getAdditionalPropertiesType(
+            PojoName.ofNameAndSuffix("Map", "Dto"));
+
+    assertTrue(additionalPropertiesSchema.isAllowed());
+    assertEquals(NoType.create(), additionalPropertiesType);
+  }
+
+  @Test
+  void wrapNullable_when_false_then_additionalPropertiesNotAllowed() {
+    final AdditionalPropertiesSchema additionalPropertiesSchema =
+        AdditionalPropertiesSchema.wrapNullable(false);
+
+    assertFalse(additionalPropertiesSchema.isAllowed());
+  }
+
+  @Test
+  void wrapNullable_when_schema_then_additionalPropertiesAllowedAndCorrectType() {
+    final ObjectSchema objectSchema = new ObjectSchema();
+    objectSchema.setProperties(Collections.emptyMap());
+
+    final AdditionalPropertiesSchema additionalPropertiesSchema =
+        AdditionalPropertiesSchema.wrapNullable(objectSchema);
+
+    final PojoName pojoName = PojoName.ofNameAndSuffix("User", "Dto");
+    final Type additionalPropertiesType =
+        additionalPropertiesSchema.getAdditionalPropertiesType(pojoName);
+
+    assertTrue(additionalPropertiesSchema.isAllowed());
+    assertEquals(
+        ObjectType.ofName(PojoName.ofNameAndSuffix("UserProperties", "Dto")),
+        additionalPropertiesType);
+  }
+}
