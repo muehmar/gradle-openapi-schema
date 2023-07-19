25253bbf839688cf464309363d9b75ea269abc3c
Martin Mühlemann <poelsi@gmail.com>
2023-07-19 18:13:47 +0200

    Fix composed properties getters
    #99
    #100

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/ComposedPropertiesGetter.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/ComposedPropertiesGetter.java
@@ -2,12 +2,12 @@ package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter;
 
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.RefsGenerator.fieldRefs;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.RefsGenerator.ref;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonIncludeNonNull;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonProperty;
 import static com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember.MemberType.ANY_OF_MEMBER;
 import static com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember.MemberType.ONE_OF_MEMBER;
-import static com.github.muehmar.gradle.openapi.util.Booleans.not;
 
 import com.github.muehmar.gradle.openapi.generator.java.OpenApiUtilRefs;
-import com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator;
 import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
@@ -19,15 +19,11 @@ class ComposedPropertiesGetter {
 
   public static Generator<JavaPojoMember, PojoSettings> composedPropertiesGetterGenerator() {
     return Generator.<JavaPojoMember, PojoSettings>emptyGen()
-        .append(notNullAnnotation())
+        .append(jsonProperty())
+        .append(jsonIncludeNonNull())
         .append(method())
         .append(fieldRefs())
         .filter(ComposedPropertiesGetter::composedPropertiesGetterFilter);
-  }
-
-  private static Generator<JavaPojoMember, PojoSettings> notNullAnnotation() {
-    return JacksonAnnotationGenerator.<JavaPojoMember>jsonIncludeNonNull()
-        .filter(JavaPojoMember::isOptional);
   }
 
   private static boolean composedPropertiesGetterFilter(
@@ -48,16 +44,33 @@ class ComposedPropertiesGetter {
   }
 
   private static String returnType(JavaPojoMember member) {
-    return member.isOptionalAndNullable()
-        ? "Object"
-        : member.getJavaType().getFullClassName().asString();
+    return member.isNullable() ? "Object" : member.getJavaType().getFullClassName().asString();
   }
 
   private static Generator<JavaPojoMember, PojoSettings> methodContent() {
-    return normalGetterMethodContent().append(getOptionalNullableGetter());
+    return notNullableGetterContent()
+        .append(requiredNullableGetterContent())
+        .append(optionalNullableGetterContent());
   }
 
-  private static Generator<JavaPojoMember, PojoSettings> getOptionalNullableGetter() {
+  private static Generator<JavaPojoMember, PojoSettings> notNullableGetterContent() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append((m, s, w) -> w.println("return %s;", m.getNameAsIdentifier()))
+        .filter(JavaPojoMember::isNotNullable);
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> requiredNullableGetterContent() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(
+            (m, s, w) ->
+                w.println(
+                    "return %s ? new JacksonNullContainer<>(%s) : null;",
+                    m.getIsPresentFlagName(), m.getNameAsIdentifier()))
+        .append(ref(OpenApiUtilRefs.JACKSON_NULL_CONTAINER))
+        .filter(JavaPojoMember::isRequiredAndNullable);
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> optionalNullableGetterContent() {
     return Generator.<JavaPojoMember, PojoSettings>emptyGen()
         .append(
             (m, s, w) ->
@@ -66,11 +79,5 @@ class ComposedPropertiesGetter {
                     m.getIsNullFlagName(), m.getNameAsIdentifier(), m.getNameAsIdentifier()))
         .append(ref(OpenApiUtilRefs.JACKSON_NULL_CONTAINER))
         .filter(JavaPojoMember::isOptionalAndNullable);
-  }
-
-  private static Generator<JavaPojoMember, PojoSettings> normalGetterMethodContent() {
-    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
-        .append((m, s, w) -> w.println("return %s;", m.getNameAsIdentifier()))
-        .filter(m -> not(m.isOptionalAndNullable()));
   }
 }
