8d8bce41ffd00e8551e0adc2909740894e76aea1
Martin Mühlemann <poelsi@gmail.com>
2023-07-31 21:12:47 +0200

    Use basic validation method for validating against the schema
    #99
    #100

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/composition/ValidationMethodGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/composition/ValidationMethodGenerator.java
@@ -1,22 +1,18 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.composition;
 
-import static com.github.muehmar.gradle.openapi.util.Booleans.not;
 import static io.github.muehmar.codegenerator.Generator.newLine;
 import static io.github.muehmar.codegenerator.java.JavaModifier.PRIVATE;
 
 import ch.bluecare.commons.data.NonEmptyList;
 import ch.bluecare.commons.data.PList;
 import com.github.muehmar.gradle.openapi.generator.java.generator.shared.Filters;
-import com.github.muehmar.gradle.openapi.generator.java.model.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.java.model.composition.JavaAnyOfComposition;
 import com.github.muehmar.gradle.openapi.generator.java.model.composition.JavaOneOfComposition;
 import com.github.muehmar.gradle.openapi.generator.java.model.pojo.JavaObjectPojo;
 import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
 import io.github.muehmar.codegenerator.Generator;
 import io.github.muehmar.codegenerator.java.MethodGenBuilder;
-import java.util.Optional;
 import java.util.function.Function;
-import lombok.Value;
 
 public class ValidationMethodGenerator {
   private ValidationMethodGenerator() {}
@@ -53,69 +49,10 @@ public class ValidationMethodGenerator {
   }
 
   private static Generator<JavaObjectPojo, PojoSettings> isValidAgainstMethodContent() {
-    return isValidAgainstMethodContentForPojoWithRequiredMember()
-        .append(isValidAgainstMethodContentForPojoWithoutRequiredMember());
-  }
-
-  private static Generator<JavaObjectPojo, PojoSettings>
-      isValidAgainstMethodContentForPojoWithRequiredMember() {
-    return Generator.<JavaObjectPojo, PojoSettings>ofWriterFunction(w -> w.println("return"))
-        .appendList(
-            singlePojoMemberCondition(),
-            pojo -> pojo.getAllMembers().map(member -> new JavaPojoAndMember(pojo, member)))
-        .filter(ValidationMethodGenerator::hasRequiredInAllMembers);
-  }
-
-  private static Generator<JavaObjectPojo, PojoSettings>
-      isValidAgainstMethodContentForPojoWithoutRequiredMember() {
-    return Generator.<JavaObjectPojo, PojoSettings>ofWriterFunction(w -> w.println("return true;"))
-        .filter(ValidationMethodGenerator::hasNoRequiredInAllMembers);
-  }
-
-  private static Generator<JavaPojoAndMember, PojoSettings> singlePojoMemberCondition() {
-    final Generator<JavaPojoAndMember, PojoSettings> requiredNotNullableGen =
-        (pm, s, w) ->
-            w.println("%s != null%s", pm.member.getNameAsIdentifier(), pm.ampersandOrSemicolon());
-    final Generator<JavaPojoAndMember, PojoSettings> requiredNullableGen =
-        (pm, s, w) ->
-            w.println("%s%s", pm.member.getIsPresentFlagName(), pm.ampersandOrSemicolon());
-    return Generator.<JavaPojoAndMember, PojoSettings>emptyGen()
-        .appendConditionally(JavaPojoAndMember::isRequiredAndNotNullable, requiredNotNullableGen)
-        .appendConditionally(JavaPojoAndMember::isRequiredAndNullable, requiredNullableGen)
-        .indent(1);
-  }
-
-  private static boolean hasRequiredInAllMembers(JavaObjectPojo pojo) {
-    return pojo.getAllMembers().exists(JavaPojoMember::isRequired);
-  }
-
-  private static boolean hasNoRequiredInAllMembers(JavaObjectPojo pojo) {
-    return not(hasRequiredInAllMembers(pojo));
-  }
-
-  @Value
-  private static class JavaPojoAndMember {
-    JavaObjectPojo pojo;
-    JavaPojoMember member;
-
-    boolean isRequiredAndNotNullable() {
-      return member.isRequiredAndNotNullable();
-    }
-
-    public boolean isRequiredAndNullable() {
-      return member.isRequiredAndNullable();
-    }
-
-    boolean isLast() {
-      return pojo.getAllMembers()
-          .filter(JavaPojoMember::isRequired)
-          .reverse()
-          .headOption()
-          .equals(Optional.of(member));
-    }
-
-    String ampersandOrSemicolon() {
-      return isLast() ? ";" : " &&";
-    }
+    return Generator.<JavaObjectPojo, PojoSettings>emptyGen()
+        .append(
+            (p, s, w) ->
+                w.println(
+                    "return %s().validateBasic();", CompositionNames.asConversionMethodName(p)));
   }
 }
