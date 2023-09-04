39bbd8b174f20f22f611d20ba6e3a17645aebfd2
Martin Mühlemann <poelsi@gmail.com>
2023-09-04 20:46:14 +0200

    Add full builder tests
    #111

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/additionalproperties/RequiredAdditionalPropertiesTest.java
@@ -0,0 +1,76 @@
+package com.github.muehmar.gradle.openapi.additionalproperties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muehmar.gradle.openapi.util.MapperFactory;
+import com.github.muehmar.gradle.openapi.util.ValidationUtil;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import openapischema.example.api.additionalproperties.model.RequiredAdditionalPropertiesDto;
+import org.junit.jupiter.api.Test;
+
+class RequiredAdditionalPropertiesTest {
+  private static final ObjectMapper MAPPER = MapperFactory.mapper();
+
+  @Test
+  void builder_when_createAndSerialized_then_correctJson() throws JsonProcessingException {
+    final RequiredAdditionalPropertiesDto dto =
+        RequiredAdditionalPropertiesDto.builder()
+            .setName("name")
+            .setLastname("lastname")
+            .andAllOptionals()
+            .addAdditionalProperty("street", "waldweg")
+            .build();
+
+    assertEquals(
+        "{\"name\":\"name\",\"street\":\"waldweg\",\"lastname\":\"lastname\"}",
+        MAPPER.writeValueAsString(dto));
+  }
+
+  @Test
+  void fullBuilder_when_createAndSerialized_then_correctJson() throws JsonProcessingException {
+    final RequiredAdditionalPropertiesDto dto =
+        RequiredAdditionalPropertiesDto.fullBuilder()
+            .setName("name")
+            .setLastname("lastname")
+            .addAdditionalProperty("street", "waldweg")
+            .build();
+
+    assertEquals(
+        "{\"name\":\"name\",\"street\":\"waldweg\",\"lastname\":\"lastname\"}",
+        MAPPER.writeValueAsString(dto));
+  }
+
+  @Test
+  void deserialize_when_json_then_correctDto() throws JsonProcessingException {
+    final RequiredAdditionalPropertiesDto exptectedDto =
+        RequiredAdditionalPropertiesDto.fullBuilder()
+            .setName("name")
+            .setLastname("lastname")
+            .addAdditionalProperty("street", "waldweg")
+            .build();
+
+    final RequiredAdditionalPropertiesDto dto =
+        MAPPER.readValue(
+            "{\"name\":\"name\",\"street\":\"waldweg\",\"lastname\":\"lastname\"}",
+            RequiredAdditionalPropertiesDto.class);
+
+    assertEquals(exptectedDto, dto);
+  }
+
+  @Test
+  void validate_when_requiredAdditionalPropertyMissing_then_violation()
+      throws JsonProcessingException {
+    final RequiredAdditionalPropertiesDto dto =
+        MAPPER.readValue(
+            "{\"name\":\"name\",\"street\":\"waldweg\"}", RequiredAdditionalPropertiesDto.class);
+
+    final Set<ConstraintViolation<RequiredAdditionalPropertiesDto>> violations =
+        ValidationUtil.validate(dto);
+
+    assertEquals(1, violations.size());
+    assertEquals("must not be null", violations.stream().findFirst().get().getMessage());
+  }
+}
