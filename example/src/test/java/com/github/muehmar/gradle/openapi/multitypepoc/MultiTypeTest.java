0bf924418a2e19a670963e3e6d0d59631f18add9
Martin Mühlemann <poelsi@gmail.com>
2023-09-26 19:31:48 +0200

    MultiType POC

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/MultiTypeTest.java
@@ -0,0 +1,39 @@
+package com.github.muehmar.gradle.openapi.multitypepoc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muehmar.gradle.openapi.util.MapperFactory;
+import org.junit.jupiter.api.Test;
+
+class MultiTypeTest {
+  private static final ObjectMapper MAPPER = MapperFactory.mapper();
+
+  @Test
+  void readValue_when_superObjectWithStringValue_then_ok() throws JsonProcessingException {
+    final String json = "{\"feature\":\"striiiing\",\"hello\":\"hello\"}";
+    final SuperObject superObject = MAPPER.readValue(json, SuperObject.class);
+    assertEquals(
+        "SuperObject{feature=MultiType{stringValue='striiiing', longValue=null, booleanValue=null}, hello='hello'}",
+        superObject.toString());
+  }
+
+  @Test
+  void readValue_then_superObjectWithLongValue_then_ok() throws JsonProcessingException {
+    final String json = "{\"feature\":123,\"hello\":\"hello\"}";
+    final SuperObject superObject = MAPPER.readValue(json, SuperObject.class);
+    assertEquals(
+        "SuperObject{feature=MultiType{stringValue='null', longValue=123, booleanValue=null}, hello='hello'}",
+        superObject.toString());
+  }
+
+  @Test
+  void readValue_then_superObjectWithBooleanValue_then_ok() throws JsonProcessingException {
+    final String json = "{\"feature\":true,\"hello\":\"hello\"}";
+    final SuperObject superObject = MAPPER.readValue(json, SuperObject.class);
+    assertEquals(
+        "SuperObject{feature=MultiType{stringValue='null', longValue=null, booleanValue=true}, hello='hello'}",
+        superObject.toString());
+  }
+}
