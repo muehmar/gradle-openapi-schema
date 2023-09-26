65631beea489188a1e96aaf420983f86f7f53bf0
Martin Mühlemann <poelsi@gmail.com>
2023-09-26 19:31:48 +0200

    MultiType POC

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/SuperObject.java
@@ -0,0 +1,41 @@
+package com.github.muehmar.gradle.openapi.multitypepoc;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+
+public class SuperObject {
+  private final MultiType feature;
+  private final String hello;
+
+  public SuperObject(
+      @JsonProperty("feature") MultiType feature, @JsonProperty("hello") String hello) {
+    this.feature = feature;
+    this.hello = hello;
+  }
+
+  public MultiType getFeature() {
+    return feature;
+  }
+
+  public String getHello() {
+    return hello;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final SuperObject that = (SuperObject) o;
+    return Objects.equals(feature, that.feature) && Objects.equals(hello, that.hello);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(feature, hello);
+  }
+
+  @Override
+  public String toString() {
+    return "SuperObject{" + "feature=" + feature + ", hello='" + hello + '\'' + '}';
+  }
+}
