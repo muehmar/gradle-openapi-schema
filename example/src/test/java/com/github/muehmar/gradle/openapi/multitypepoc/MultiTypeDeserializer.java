1329c86ae3a03e8e007fb965cccbacb403588fc2
Martin Mühlemann <poelsi@gmail.com>
2023-10-08 21:38:56 +0200

    Update

--- example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/MultiTypeDeserializer.java
+++ example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/MultiTypeDeserializer.java
@@ -5,9 +5,17 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.BinaryNode;
 import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.FloatNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.LongNode;
 import com.fasterxml.jackson.databind.node.NumericNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
+import com.fasterxml.jackson.databind.util.TokenBuffer;
 import java.io.IOException;
 
 public class MultiTypeDeserializer extends StdDeserializer<MultiType> {
@@ -18,33 +26,62 @@ public class MultiTypeDeserializer extends StdDeserializer<MultiType> {
   @Override
   public MultiType deserialize(JsonParser p, DeserializationContext ctxt)
       throws IOException, JacksonException {
+
+    // Necessary if objects need to be parsed
+    final TokenBuffer tokenBuffer = ctxt.bufferAsCopyOfValue(p);
+
     final TreeNode treeNode = p.readValueAsTree();
 
-    try {
-      if (treeNode instanceof BooleanNode) {
-        final Boolean b = ((BooleanNode) treeNode).booleanValue();
-        return MultiType.fromBoolean(b);
+    if (treeNode instanceof ValueNode) {
+      final ValueNode valueNode = (ValueNode) treeNode;
+
+      if (valueNode instanceof NumericNode) {
+        final NumericNode numericNode = (NumericNode) valueNode;
+
+        if (numericNode instanceof IntNode) {
+          // Parse integers
+        }
+
+        if (numericNode instanceof LongNode) {
+          return MultiType.fromLong(tokenBuffer.asParser().readValueAs(Long.class));
+        }
+
+        if (numericNode instanceof FloatNode) {
+          // Parse float
+        }
+
+        if (numericNode instanceof DoubleNode) {
+          // Parse double
+        }
+
+        // Parse directly into integer/long or float/double if possible
+        try {
+          return MultiType.fromLong(tokenBuffer.asParser().readValueAs(Long.class));
+        } catch (Exception e) {
+          // Try next
+        }
       }
-    } catch (Exception e) {
-      System.err.println(e);
+
+      if (valueNode instanceof BinaryNode) {
+        // Parse binary?
+      }
+
+      if (valueNode instanceof TextNode) {
+        return MultiType.fromString(tokenBuffer.asParser().readValueAs(String.class));
+      }
+
+      if (valueNode instanceof BooleanNode) {
+        return MultiType.fromBoolean(tokenBuffer.asParser().readValueAs(Boolean.class));
+      }
     }
 
-    try {
-      if (treeNode instanceof NumericNode) {
-        final Long l = ((NumericNode) treeNode).asLong();
-        return MultiType.fromLong(l);
+    if (treeNode instanceof ObjectNode) {
+      // Parse objects
+      try {
+        // tokenBuffer.asParser().readValueAs(ObjectDto.class);
+      } catch (Exception e) {
+        // Try next object
       }
-    } catch (Exception e) {
-      System.err.println(e);
-    }
-
-    try {
-      if (treeNode instanceof TextNode) {
-        final String s = ((TextNode) treeNode).textValue();
-        return MultiType.fromString(s);
-      }
-    } catch (Exception e) {
-      System.err.println(e);
     }
 
     throw new IllegalArgumentException("Could not deserialize MultiType");
