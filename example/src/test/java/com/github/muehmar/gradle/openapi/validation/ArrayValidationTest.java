464cb159c7ef403bb15ed932327b89a7486f892b
Martin Mühlemann <poelsi@gmail.com>
2023-10-29 21:34:37 +0100

    Add tests for array property validation
    Issue: #174

--- example/src/test/java/com/github/muehmar/gradle/openapi/validation/ArrayValidationTest.java
+++ example/src/test/java/com/github/muehmar/gradle/openapi/validation/ArrayValidationTest.java
@@ -7,7 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.validation.ConstraintViolation;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -22,7 +24,14 @@ class ArrayValidationTest {
       numbers.add(i);
     }
 
-    final IntegerMemberArrayDto dto = IntegerMemberArrayDto.builder().setNumbers(numbers).build();
+    final IntegerMemberArrayDto dto =
+        IntegerMemberArrayDto.builder()
+            .setNumbers1(numbers)
+            .setNumbers2(numbers)
+            .andAllOptionals()
+            .setNumbers3(numbers)
+            .setNumbers4(numbers)
+            .build();
 
     final Set<ConstraintViolation<IntegerMemberArrayDto>> constraintViolations = validate(dto);
 
@@ -38,14 +47,23 @@ class ArrayValidationTest {
       numbers.add(i);
     }
 
-    final IntegerMemberArrayDto dto = IntegerMemberArrayDto.builder().setNumbers(numbers).build();
+    final IntegerMemberArrayDto dto =
+        IntegerMemberArrayDto.builder()
+            .setNumbers1(numbers)
+            .setNumbers2(numbers)
+            .andAllOptionals()
+            .setNumbers3(numbers)
+            .setNumbers4(numbers)
+            .build();
 
     final Set<ConstraintViolation<IntegerMemberArrayDto>> constraintViolations = validate(dto);
 
-    assertEquals(1, constraintViolations.size());
+    assertEquals(4, constraintViolations.size());
     assertEquals(
-        "size must be between 3 and 5",
-        constraintViolations.stream().findFirst().get().getMessage());
+        Collections.singleton("size must be between 3 and 5"),
+        constraintViolations.stream()
+            .map(ConstraintViolation::getMessage)
+            .collect(Collectors.toSet()));
     assertFalse(dto.isValid());
   }
 
@@ -85,7 +103,7 @@ class ArrayValidationTest {
   }
 
   @Test
-  void validate_when_singleValueInArrayViolatesMaximum_then_violation() {
+  void validate_when_singleValueInPojoArrayViolatesMaximumIn_then_violation() {
     final ArrayList<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3, 150));
     final IntegerPojoArrayDto dto = new IntegerPojoArrayDto(integers);
 
@@ -94,6 +112,28 @@ class ArrayValidationTest {
     assertEquals(1, violations.size());
     assertEquals(
         "must be less than or equal to 100", violations.stream().findFirst().get().getMessage());
+    assertFalse(dto.isValid());
+  }
+
+  @Test
+  void validate_when_singleValueInMemberArrayViolatesMaximumIn_then_violation() {
+    final ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 150));
+
+    final IntegerMemberArrayDto dto =
+        IntegerMemberArrayDto.builder()
+            .setNumbers1(numbers)
+            .setNumbers2(numbers)
+            .andAllOptionals()
+            .setNumbers3(numbers)
+            .setNumbers4(numbers)
+            .build();
+
+    final Set<ConstraintViolation<IntegerMemberArrayDto>> violations = validate(dto);
+
+    assertEquals(4, violations.size());
+    assertEquals(
+        Collections.singleton("must be less than or equal to 100"),
+        violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet()));
     assertFalse(dto.isValid());
   }
 
