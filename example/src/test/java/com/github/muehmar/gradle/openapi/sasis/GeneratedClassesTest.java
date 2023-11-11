3040ae8dcac2b7880b74f027c365f7d182525005
Martin Mühlemann <poelsi@gmail.com>
2023-11-11 08:35:10 +0100

    Verify generated class names

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/sasis/GeneratedClassesTest.java
@@ -0,0 +1,22 @@
+package com.github.muehmar.gradle.openapi.sasis;
+
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import com.github.muehmar.gradle.openapi.snapshot.SnapshotTest;
+import com.github.muehmar.gradle.openapi.util.ClassFinder;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+@SnapshotTest
+class GeneratedClassesTest {
+  private Expect expect;
+
+  @Test
+  @SnapshotName("allGeneratedClasses")
+  void findNonTestClassesInPackage_when_modelPackage_then_allClasses() {
+    final List<String> classNames =
+        ClassFinder.findNonTestClassesInPackage(getClass().getPackage().getName());
+
+    expect.toMatchSnapshot(String.join("\n", classNames));
+  }
+}
