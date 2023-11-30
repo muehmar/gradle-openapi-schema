5d83ff7493ca9d705650a42d2b9270bc45acf292
Martin Mühlemann <poelsi@gmail.com>
2023-11-30 21:23:01 +0100

    Add tests for issues

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/issue190/Issue190Test.java
@@ -0,0 +1,22 @@
+package com.github.muehmar.gradle.openapi.issue190;
+
+import static com.github.muehmar.gradle.openapi.issue190.Leaf1ADto.leaf1ADtoBuilder;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class Issue190Test {
+
+  @Test
+  void generatedCodeCompiles() {
+    final Root2Dto dto =
+        Root2Dto.root2DtoBuilder()
+            .setLeaf1ADto(leaf1ADtoBuilder().build())
+            .andAllOptionals()
+            .setProp1(1)
+            .setProp2(2)
+            .build();
+
+    assertNotNull(dto);
+  }
+}
