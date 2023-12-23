8fe965b0a3cdf2e854628941d4b8e510ebf237bb
Martin Mühlemann <poelsi@gmail.com>
2023-12-23 13:03:48 +0100

    Support only enum types defined in parent schema
    Issue: #219

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/model/composition/OneOfComposition.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/model/composition/OneOfComposition.java
@@ -32,12 +32,14 @@ public class OneOfComposition {
   public Optional<Discriminator> determineDiscriminator(
       Optional<UntypedDiscriminator> objectPojoDiscriminator) {
     if (objectPojoDiscriminator.isPresent()) {
-      return typeDiscriminator(pojos, objectPojoDiscriminator);
+      return typeDiscriminator(
+          pojos, objectPojoDiscriminator, DiscriminatorDefinitionOrigin.PARENT_SCHEMA);
     }
 
     final Optional<UntypedDiscriminator> discriminatorFromPojos =
         determineDiscriminatorFromComposedPojos();
-    return typeDiscriminator(pojos, discriminatorFromPojos);
+    return typeDiscriminator(
+        pojos, discriminatorFromPojos, DiscriminatorDefinitionOrigin.EACH_COMPOSITION_SCHEMA);
   }
 
   private Optional<UntypedDiscriminator> determineDiscriminatorFromComposedPojos() {
@@ -84,12 +86,16 @@ public class OneOfComposition {
   }
 
   private static Optional<Discriminator> typeDiscriminator(
-      NonEmptyList<Pojo> pojos, Optional<UntypedDiscriminator> discriminator) {
-    return discriminator.map(disc -> typeDiscriminator(pojos, disc));
+      NonEmptyList<Pojo> pojos,
+      Optional<UntypedDiscriminator> discriminator,
+      DiscriminatorDefinitionOrigin discriminatorOrigin) {
+    return discriminator.map(disc -> typeDiscriminator(pojos, disc, discriminatorOrigin));
   }
 
   private static Discriminator typeDiscriminator(
-      NonEmptyList<Pojo> pojos, UntypedDiscriminator discriminator) {
+      NonEmptyList<Pojo> pojos,
+      UntypedDiscriminator discriminator,
+      DiscriminatorDefinitionOrigin discriminatorOrigin) {
     final Name propertyName = discriminator.getPropertyName();
     final NonEmptyList<DiscriminatorType> types =
         pojos
@@ -101,7 +107,7 @@ public class OneOfComposition {
                                 new OpenApiGeneratorException(
                                     "Only schemas of type object are supported for compositions, but %s is not of type object",
                                     pojo.getName().getSchemaName())))
-            .map(objectPojo -> getDiscriminatorType(objectPojo, propertyName));
+            .map(objectPojo -> getDiscriminatorType(objectPojo, propertyName, discriminatorOrigin));
 
     final boolean allSameType = types.toPList().forall(types.head()::equals);
     if (not(allSameType)) {
@@ -113,7 +119,8 @@ public class OneOfComposition {
     return Discriminator.typeDiscriminator(discriminator, types.head());
   }
 
-  private static DiscriminatorType getDiscriminatorType(ObjectPojo objectPojo, Name propertyName) {
+  private static DiscriminatorType getDiscriminatorType(
+      ObjectPojo objectPojo, Name propertyName, DiscriminatorDefinitionOrigin discriminatorOrigin) {
     final SchemaName schemaName = objectPojo.getName().getSchemaName();
     final PojoMember discriminatorMember =
         objectPojo
@@ -125,10 +132,11 @@ public class OneOfComposition {
                         "Invalid schema: Pojo %s does not have a property named %s used by the discriminator.",
                         schemaName, propertyName));
     assertNecessity(schemaName, discriminatorMember);
-    return extractType(schemaName, discriminatorMember);
+    return extractType(schemaName, discriminatorMember, discriminatorOrigin);
   }
 
-  private static DiscriminatorType extractType(SchemaName schemaName, PojoMember member) {
+  private static DiscriminatorType extractType(
+      SchemaName schemaName, PojoMember member, DiscriminatorDefinitionOrigin discriminatorOrigin) {
     final Type type = member.getType();
 
     final Optional<DiscriminatorType> stringTypeDiscriminator =
@@ -138,6 +146,13 @@ public class OneOfComposition {
 
     final Optional<DiscriminatorType> enumTypeDiscriminator =
         type.asEnumType().map(DiscriminatorType::fromEnumType);
+
+    if (enumTypeDiscriminator.isPresent()
+        && discriminatorOrigin.equals(DiscriminatorDefinitionOrigin.EACH_COMPOSITION_SCHEMA)) {
+      throw new OpenApiGeneratorException(
+          "An enum as discriminator (property %s for schema %s) is only supported if it is defined in a single parent schema.",
+          member.getName(), schemaName);
+    }
 
     return Optionals.or(stringTypeDiscriminator, enumTypeDiscriminator)
         .orElseThrow(
@@ -193,5 +208,13 @@ public class OneOfComposition {
                 return Optional.empty();
               });
     }
+  }
+
+  private enum DiscriminatorDefinitionOrigin {
+    /** Defined in a single parent schema and inherited via allOf composition */
+    PARENT_SCHEMA,
+
+    /** Defined in each single schema used in the composition. */
+    EACH_COMPOSITION_SCHEMA,
   }
 }
