8d039f4f870b2877918e8a211a9ed6b543948ba6
Martin Mühlemann <poelsi@gmail.com>
2024-02-07 17:31:32 +0100

    Add tests for issue #248
    Issue: #248

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/issue248/Issue248Test.java
@@ -0,0 +1,20 @@
+package com.github.muehmar.gradle.openapi.issue248;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.github.muehmar.openapi.util.Tristate;
+import org.junit.jupiter.api.Test;
+
+public class Issue248Test {
+  @Test
+  void fullPersonDtoBuilder_when_used_then_firstNameIsTristate() {
+    final PersonDto personDto =
+        PersonDto.fullPersonDtoBuilder()
+            .setFirstName(Tristate.ofValue("John"))
+            .setLastName(Tristate.ofValue("Doe"))
+            .build();
+
+    assertEquals(Tristate.ofValue("John"), personDto.getFirstNameTristate());
+    assertEquals(Tristate.ofValue("Doe"), personDto.getLastNameTristate());
+  }
+}
