e5f42b6f59ac30064875dec58e0ff769ca5bc03a
Martin Mühlemann <poelsi@gmail.com>
2024-02-27 21:15:12 +0100

    Overload additional properties setter
    Issue: #158

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/AdditionalPropertiesSetterGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/AdditionalPropertiesSetterGenerator.java
@@ -2,7 +2,10 @@ package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder;
 
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.RefsGenerator.ref;
 import static com.github.muehmar.gradle.openapi.generator.java.model.JavaAdditionalProperties.additionalPropertiesName;
+import static com.github.muehmar.gradle.openapi.generator.java.ref.JavaRefs.JAVA_UTIL_OPTIONAL;
+import static com.github.muehmar.gradle.openapi.generator.java.ref.OpenApiUtilRefs.TRISTATE;
 import static com.github.muehmar.gradle.openapi.util.Booleans.not;
+import static io.github.muehmar.codegenerator.Generator.constant;
 import static io.github.muehmar.codegenerator.java.JavaModifier.PRIVATE;
 import static io.github.muehmar.codegenerator.java.JavaModifier.PUBLIC;
 import static io.github.muehmar.codegenerator.java.MethodGen.Argument.argument;
@@ -33,15 +36,19 @@ class AdditionalPropertiesSetterGenerator {
         .appendSingleBlankLine()
         .append(singleAdditionalPropertiesSetter(false))
         .appendSingleBlankLine()
+        .append(singleOptionalAdditionalPropertiesSetter())
+        .appendSingleBlankLine()
+        .append(singleTristateAdditionalPropertiesSetter())
+        .appendSingleBlankLine()
         .append(allAdditionalPropertiesSetter());
   }
 
   private static Generator<JavaAdditionalProperties, PojoSettings> singleAdditionalPropertiesSetter(
-      boolean forObjectType) {
+      boolean forAnyType) {
     final Generator<JavaAdditionalProperties, PojoSettings> method =
         MethodGenBuilder.<JavaAdditionalProperties, PojoSettings>create()
             .modifiers(
-                props -> createModifiersForSingleAdditionalPropertiesSetter(props, forObjectType))
+                props -> createModifiersForSingleAdditionalPropertiesSetter(props, forAnyType))
             .noGenericTypes()
             .returnType("Builder")
             .methodName("addAdditionalProperty")
@@ -50,7 +57,7 @@ class AdditionalPropertiesSetterGenerator {
                     PList.of(
                         new Argument("String", "key"),
                         new Argument(
-                            forObjectType
+                            forAnyType
                                 ? "Object"
                                 : props.getType().getParameterizedClassName().asString(),
                             "value")))
@@ -62,14 +69,76 @@ class AdditionalPropertiesSetterGenerator {
             .build()
             .append(RefsGenerator.javaTypeRefs(), JavaAdditionalProperties::getType);
     return JacksonAnnotationGenerator.<JavaAdditionalProperties>jsonAnySetter()
-        .filter(ignore -> not(forObjectType))
+        .filter(ignore -> not(forAnyType))
         .append(method);
   }
 
   private static JavaModifiers createModifiersForSingleAdditionalPropertiesSetter(
-      JavaAdditionalProperties props, boolean forObjectType) {
-    final boolean privateMethod = forObjectType || not(props.isAllowed());
+      JavaAdditionalProperties props, boolean forAnyType) {
+    final boolean privateMethod = forAnyType || not(props.isAllowed());
     return JavaModifiers.of(privateMethod ? PRIVATE : PUBLIC);
+  }
+
+  private static Generator<JavaAdditionalProperties, PojoSettings>
+      singleOptionalAdditionalPropertiesSetter() {
+    return MethodGenBuilder.<JavaAdditionalProperties, PojoSettings>create()
+        .modifiers(PUBLIC)
+        .noGenericTypes()
+        .returnType("Builder")
+        .methodName("addAdditionalProperty")
+        .arguments(
+            props ->
+                PList.of(
+                    new Argument("String", "key"),
+                    new Argument(
+                        "Optional<" + props.getType().getParameterizedClassName().asString() + ">",
+                        "value")))
+        .doesNotThrow()
+        .content(singleOptionalAdditionalPropertiesSetterContent())
+        .build()
+        .append(RefsGenerator.javaTypeRefs(), JavaAdditionalProperties::getType)
+        .append(ref(JAVA_UTIL_OPTIONAL))
+        .filter(props -> props.getType().getNullability().isNotNullable());
+  }
+
+  private static Generator<JavaAdditionalProperties, PojoSettings>
+      singleOptionalAdditionalPropertiesSetterContent() {
+    return Generator.<JavaAdditionalProperties, PojoSettings>emptyGen()
+        .append(
+            constant("value.ifPresent(val -> this.%s.put(key, val));", additionalPropertiesName()))
+        .append(constant("return this;"));
+  }
+
+  private static Generator<JavaAdditionalProperties, PojoSettings>
+      singleTristateAdditionalPropertiesSetter() {
+    return MethodGenBuilder.<JavaAdditionalProperties, PojoSettings>create()
+        .modifiers(PUBLIC)
+        .noGenericTypes()
+        .returnType("Builder")
+        .methodName("addAdditionalProperty")
+        .arguments(
+            props ->
+                PList.of(
+                    new Argument("String", "key"),
+                    new Argument(
+                        "Tristate<" + props.getType().getParameterizedClassName().asString() + ">",
+                        "value")))
+        .doesNotThrow()
+        .content(singleTristateAdditionalPropertiesSetterContent())
+        .build()
+        .append(RefsGenerator.javaTypeRefs(), JavaAdditionalProperties::getType)
+        .append(ref(TRISTATE))
+        .filter(props -> props.getType().getNullability().isNullable());
+  }
+
+  private static Generator<JavaAdditionalProperties, PojoSettings>
+      singleTristateAdditionalPropertiesSetterContent() {
+    return Generator.<JavaAdditionalProperties, PojoSettings>emptyGen()
+        .append(constant("value"))
+        .append(constant(".onValue(val -> this.%s.put(key, val))", additionalPropertiesName()), 2)
+        .append(constant(".onNull(() -> this.%s.put(key, null))", additionalPropertiesName()), 2)
+        .append(constant(".onAbsent(() -> null);", additionalPropertiesName()), 2)
+        .append(constant("return this;"));
   }
 
   private static Generator<JavaAdditionalProperties, PojoSettings> allAdditionalPropertiesSetter() {
