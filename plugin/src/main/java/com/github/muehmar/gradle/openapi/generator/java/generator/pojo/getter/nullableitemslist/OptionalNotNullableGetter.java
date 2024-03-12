0a640f3d1ba3851292ae0eb408f724015b27c311
Martin Mühlemann <poelsi@gmail.com>
2024-03-12 21:38:08 +0100

    Implement OptionalNotNullableGetter for lists
    Issue: #158

---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/nullableitemslist/OptionalNotNullableGetter.java
@@ -0,0 +1,57 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.nullableitemslist;
+
+import static com.github.muehmar.gradle.openapi.generator.java.GeneratorUtil.noSettingsGen;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.CommonGetter.notNullableValidationMethodWithAnnotation;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.DeprecatedMethodGenerator.deprecatedJavaDocAndAnnotationForValidationMethod;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.Filters.isJacksonJsonOrValidation;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonIgnore;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonIncludeNonNull;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonProperty;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.validation.ValidationAnnotationGenerator.validationAnnotationsForMember;
+import static io.github.muehmar.codegenerator.java.JavaDocGenerator.javaDoc;
+
+import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.CommonGetter;
+import com.github.muehmar.gradle.openapi.generator.java.model.member.JavaPojoMember;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+
+public class OptionalNotNullableGetter {
+  private OptionalNotNullableGetter() {}
+
+  public static Generator<JavaPojoMember, PojoSettings> optionalNotNullableGetter() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(standardGetter())
+        .appendSingleBlankLine()
+        .append(alternateGetter())
+        .appendSingleBlankLine()
+        .append(frameworkGetter())
+        .appendSingleBlankLine()
+        .append(notNullableValidationMethodWithAnnotation())
+        .filter(JavaPojoMember::isOptionalAndNotNullable);
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> standardGetter() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(noSettingsGen(javaDoc()), JavaPojoMember::getDescription)
+        .append(jsonIgnore())
+        .append(NullableItemsListCommonGetter.wrapNullableInOptionalGetterMethod());
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> alternateGetter() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(noSettingsGen(javaDoc()), JavaPojoMember::getDescription)
+        .append(jsonIgnore())
+        .append(NullableItemsListCommonGetter.wrapNullableInOptionalGetterOrMethod());
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> frameworkGetter() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .appendNewLine()
+        .append(deprecatedJavaDocAndAnnotationForValidationMethod())
+        .append(validationAnnotationsForMember())
+        .append(jsonProperty())
+        .append(jsonIncludeNonNull())
+        .append(CommonGetter.rawGetterMethod())
+        .filter(isJacksonJsonOrValidation());
+  }
+}
---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/nullableitemslist/OptionalNotNullableGetterTest.java
@@ -0,0 +1,46 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.nullableitemslist;
+
+import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.nullableitemslist.OptionalNotNullableGetter.optionalNotNullableGetter;
+import static com.github.muehmar.gradle.openapi.generator.model.Necessity.OPTIONAL;
+import static com.github.muehmar.gradle.openapi.generator.model.Nullability.NOT_NULLABLE;
+import static com.github.muehmar.gradle.openapi.generator.model.Nullability.NULLABLE;
+import static com.github.muehmar.gradle.openapi.generator.settings.TestPojoSettings.defaultTestSettings;
+import static com.github.muehmar.gradle.openapi.snapshot.SnapshotUtil.writerSnapshot;
+import static io.github.muehmar.codegenerator.writer.Writer.javaWriter;
+
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.annotations.SnapshotName;
+import com.github.muehmar.gradle.openapi.generator.java.model.member.JavaPojoMember;
+import com.github.muehmar.gradle.openapi.generator.java.model.member.TestJavaPojoMembers;
+import com.github.muehmar.gradle.openapi.generator.model.constraints.Constraints;
+import com.github.muehmar.gradle.openapi.generator.model.constraints.Pattern;
+import com.github.muehmar.gradle.openapi.generator.model.constraints.Size;
+import com.github.muehmar.gradle.openapi.generator.model.type.StringType;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import com.github.muehmar.gradle.openapi.snapshot.SnapshotTest;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.writer.Writer;
+import org.junit.jupiter.api.Test;
+
+@SnapshotTest
+class OptionalNotNullableGetterTest {
+  private Expect expect;
+
+  @Test
+  @SnapshotName("nullableStringItem")
+  void generate_when_nullableStringItem_then_matchSnapshot() {
+    final Generator<JavaPojoMember, PojoSettings> generator = optionalNotNullableGetter();
+    final JavaPojoMember member =
+        TestJavaPojoMembers.list(
+            StringType.noFormat()
+                .withNullability(NULLABLE)
+                .withConstraints(Constraints.ofPattern(Pattern.ofUnescapedString("pattern"))),
+            OPTIONAL,
+            NOT_NULLABLE,
+            Constraints.ofSize(Size.of(5, 10)));
+
+    final Writer writer = generator.generate(member, defaultTestSettings(), javaWriter());
+
+    expect.toMatchSnapshot(writerSnapshot(writer));
+  }
+}
