9bd2400a77862ad0824826029fddfc24a696ac4d
Martin Mühlemann <poelsi@gmail.com>
2024-05-03 15:52:39 +0200

    Change ApiRequiredNotNullableMemberSetter to ApiMemberSetter
    Issue: #160

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/FlagAssignments.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/FlagAssignments.java
@@ -1,6 +1,7 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter;
 
 import com.github.muehmar.gradle.openapi.generator.java.model.member.JavaPojoMember;
+import java.util.Optional;
 
 public class FlagAssignments {
   private FlagAssignments() {}
@@ -21,5 +22,16 @@ public class FlagAssignments {
     return String.format(
         "this.%s = %s.%s;",
         member.getIsNullFlagName(), member.getName(), member.tristateToIsNullFlag());
+  }
+
+  public static Optional<String> forStandardMemberSetter(JavaPojoMember member) {
+    if (member.isRequiredAndNullable()) {
+      return Optional.of(FlagAssignments.requiredNullableFlagAssignment(member));
+    } else if (member.isOptionalAndNotNullable()) {
+      return Optional.of(FlagAssignments.optionalNotNullableFlagAssignment(member));
+    } else if (member.isOptionalAndNullable()) {
+      return Optional.of(FlagAssignments.optionalNullableFlagAssignment(member));
+    }
+    return Optional.empty();
   }
 }
