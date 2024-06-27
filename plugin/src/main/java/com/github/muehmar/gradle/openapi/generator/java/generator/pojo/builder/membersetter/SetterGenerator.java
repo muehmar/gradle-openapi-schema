1e4832c22ea4f15011563cd5d521b300d8dce44f
Martin Mühlemann <poelsi@gmail.com>
2024-06-27 21:52:47 +0200

    Refactor list assigment and introduce ListAssigmentWriter
    Issue: #160

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/SetterGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/SetterGenerator.java
@@ -10,7 +10,6 @@ import io.github.muehmar.codegenerator.java.JavaDocGenerator;
 import io.github.muehmar.codegenerator.java.JavaModifiers;
 import io.github.muehmar.codegenerator.java.MethodGen;
 import io.github.muehmar.codegenerator.java.MethodGenBuilder;
-import io.github.muehmar.codegenerator.writer.Writer;
 
 public class SetterGenerator {
   private SetterGenerator() {}
@@ -44,18 +43,13 @@ public class SetterGenerator {
                     argument(memberSetter.argumentType(), memberSetter.getMember().getName()))
             .doesNotThrow()
             .content(
-                ((memberSetter, settings, writer) -> {
-                  final Writer valueAssigmentWriter =
-                      writer.println(
-                          "this.%s = %s;",
-                          memberSetter.getMember().getName(), memberSetter.memberValue());
-                  return memberSetter
-                      .flagAssignment()
-                      .map(valueAssigmentWriter::println)
-                      .orElse(valueAssigmentWriter)
-                      .println("return this;")
-                      .refs(memberSetter.getRefs());
-                }))
+                (memberSetter, settings, writer) ->
+                    memberSetter
+                        .flagAssignment()
+                        .map(memberSetter.memberAssigment()::println)
+                        .orElse(memberSetter.memberAssigment())
+                        .println("return this;")
+                        .refs(memberSetter.getRefs()))
             .build();
     return Generator.<MemberSetter, PojoSettings>emptyGen()
         .append(
---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/unmaplist/UnmapListMethod.java
@@ -0,0 +1,5 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.unmaplist;
+
+public class UnmapListMethod {
+  public static final String METHOD_NAME = "unmapList";
+}
