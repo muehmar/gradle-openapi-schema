7d835e80dbc9ab56925f4649aad2b230c34a90de
Martin Mühlemann <poelsi@gmail.com>
2024-06-28 16:53:49 +0200

    Replace also array item types
    Issue: #275

--- plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/model/pojo/ArrayPojoTest.java
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/model/pojo/ArrayPojoTest.java
@@ -6,10 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.github.muehmar.gradle.openapi.generator.model.Nullability;
 import com.github.muehmar.gradle.openapi.generator.model.constraints.Constraints;
 import com.github.muehmar.gradle.openapi.generator.model.constraints.Size;
+import com.github.muehmar.gradle.openapi.generator.model.name.PojoName;
 import com.github.muehmar.gradle.openapi.generator.model.type.NumericType;
+import com.github.muehmar.gradle.openapi.generator.model.type.StandardObjectType;
 import org.junit.jupiter.api.Test;
 
 class ArrayPojoTest {
+
+  @Test
+  void replaceObjectType_when_matchesItemObjectType_then_itemTypeReplacedCorrectly() {
+    final PojoName objectTypeName = PojoName.ofNameAndSuffix("PosologyItem", "Dto");
+
+    final ArrayPojo arrayPojo =
+        ArrayPojo.of(
+            componentName("Posology", "Dto"),
+            "Posology",
+            Nullability.NOT_NULLABLE,
+            StandardObjectType.ofName(objectTypeName),
+            Constraints.ofSize(Size.ofMax(4)));
+
+    final ArrayPojo adjustedArrayPojo =
+        arrayPojo.replaceObjectType(objectTypeName, "Posology item", NumericType.formatDouble());
+
+    assertEquals(NumericType.formatDouble(), adjustedArrayPojo.getItemType());
+  }
 
   @Test
   void applyMapping_when_called_then_nameMappedCorrectly() {
