cd43c834c491c6a205cb06065962287180b139fc
Martin Mühlemann <poelsi@gmail.com>
2024-07-21 17:53:58 +0200

    Fix condition for standard setters
    Issue: #160

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/standardsetters/AllMemberSetter.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/builder/membersetter/standardsetters/AllMemberSetter.java
@@ -1,12 +1,9 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.standardsetters;
 
-import static com.github.muehmar.gradle.openapi.util.Booleans.not;
-
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.FlagAssignments;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.MemberSetter;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.SetterModifier;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.SetterModifier.SetterJavaType;
-import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.builder.membersetter.apitypelist.ApiTypeListConditions;
 import com.github.muehmar.gradle.openapi.generator.java.model.member.JavaPojoMember;
 import com.github.muehmar.gradle.openapi.generator.model.Necessity;
 import com.github.muehmar.gradle.openapi.generator.model.Nullability;
@@ -29,8 +26,7 @@ class AllMemberSetter implements MemberSetter {
 
   @Override
   public boolean shouldBeUsed(PojoSettings settings) {
-    return member.getJavaType().hasNoApiType()
-        && not(ApiTypeListConditions.groupCondition().test(member));
+    return StandardSetterConditions.groupCondition().test(member);
   }
 
   @Override
