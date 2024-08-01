6cfe0d78f123aebc33f413fa80092cbba34a1abe
Martin Mühlemann <poelsi@gmail.com>
2024-08-01 07:36:42 +0200

    Implement ValidationGetter
    Issue: #160

---
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/ValidationGetter.java
@@ -0,0 +1,53 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter;
+
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.DeprecatedMethodGenerator.deprecatedJavaDocAndAnnotationForValidationMethod;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.JavaTypeGenerators.deepAnnotatedParameterizedClassName;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.jackson.JacksonAnnotationGenerator.jsonIgnore;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.shared.validation.ValidationAnnotationGenerator.validationAnnotationsForMember;
+
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.SettingsFunctions;
+import com.github.muehmar.gradle.openapi.generator.java.generator.shared.validation.ValidationAnnotationGenerator;
+import com.github.muehmar.gradle.openapi.generator.java.model.member.JavaPojoMember;
+import com.github.muehmar.gradle.openapi.generator.settings.PojoSettings;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.java.JavaGenerators;
+import java.util.function.BiFunction;
+
+public class ValidationGetter {
+  private ValidationGetter() {}
+
+  public static Generator<JavaPojoMember, PojoSettings> validationGetterGenerator() {
+    return Generator.<JavaPojoMember, PojoSettings>emptyGen()
+        .append(deprecatedJavaDocAndAnnotationForValidationMethod())
+        .append(jsonIgnore())
+        .append(validationAnnotationsForMember())
+        .append(getterMethod());
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> getterMethod() {
+    return JavaGenerators.<JavaPojoMember, PojoSettings>methodGen()
+        .modifiers(SettingsFunctions::validationMethodModifiers)
+        .noGenericTypes()
+        .returnType(returnType())
+        .methodName(methodName())
+        .noArguments()
+        .doesNotThrow()
+        .content(f -> String.format("return %s;", f.getName()))
+        .build();
+  }
+
+  private static Generator<JavaPojoMember, PojoSettings> returnType() {
+    return deepAnnotatedParameterizedClassName()
+        .contraMap(ValidationAnnotationGenerator.PropertyType::fromMember);
+  }
+
+  private static BiFunction<JavaPojoMember, PojoSettings, String> methodName() {
+    return (member, settings) -> {
+      if (member.isRequiredAndNotNullable()) {
+        return member.getGetterName().asString();
+      } else {
+        return member.getValidationGetterName(settings).asString();
+      }
+    };
+  }
+}
--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/definition/GetterMethod.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/getter/definition/GetterMethod.java
@@ -1,6 +1,7 @@
 package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.definition;
 
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.JsonGetter.jsonGetterGenerator;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.ValidationGetter.validationGetterGenerator;
 
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.FlagValidationGetter;
 import com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.FrameworkGetter;
@@ -24,11 +25,12 @@ enum GetterMethod {
   OPTIONAL_GETTER(OptionalGetter::optionalGetterGenerator),
   OPTIONAL_OR_GETTER(OptionalOrGetter::optionalOrGetterGenerator),
   TRISTATE_GETTER(TristateGetter::tristateGetterGenerator),
-  JSON_GETTER(ignore -> jsonGetterGenerator()),
   LIST_STANDARD_GETTER(ListStandardGetter::listStandardGetterGenerator),
   LIST_OPTIONAL_GETTER(ListOptionalGetter::listOptionalGetterGenerator),
   LIST_OPTIONAL_OR_GETTER(ListOptionalOrGetter::listOptionalOrGetterGenerator),
   LIST_TRISTATE_GETTER(ListTristateGetter::listTristateGetterGenerator),
+  JSON_GETTER(ignore -> jsonGetterGenerator()),
+  VALIDATION_GETTER(ignore -> validationGetterGenerator()),
   FLAG_VALIDATION_GETTER(FlagValidationGetter::flagValidationGetterGenerator);
 
   private final Function<GetterGeneratorSettings, Generator<JavaPojoMember, PojoSettings>>
