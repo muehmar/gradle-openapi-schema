5b048dc0ce2265285995eef665c8b7c8c06a28d6
Martin Mühlemann <poelsi@gmail.com>
2024-09-12 21:22:06 +0200

    Remove obsolete unwrap list methods

--- plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/ObjectPojoGenerator.java
+++ plugin/src/main/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/ObjectPojoGenerator.java
@@ -18,7 +18,6 @@ import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.ge
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.getter.additionalproperties.AdditionalPropertiesGetter.additionalPropertiesGetterGenerator;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.listmapping.ListMappingMethods.unmapListMethods;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.map.MapFactoryMethodeGenerator.mapFactoryMethodeGenerator;
-import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.nullableitemslist.NullableItemsListWrappers.nullableItemsListWrappers;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.validation.AdditionalPropertiesTypeValidationGenerator.additionalPropertiesTypeValidationGenerator;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.validation.MultipleOfValidationMethodGenerator.multipleOfValidationMethodGenerator;
 import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.validation.NoAdditionalPropertiesValidationMethodGenerator.noAdditionalPropertiesValidationMethodGenerator;
@@ -83,8 +82,6 @@ public class ObjectPojoGenerator implements Generator<JavaObjectPojo, PojoSettin
         .append(getters())
         .appendSingleBlankLine()
         .append(witherGenerator(), JavaObjectPojo::getWitherContent)
-        .appendSingleBlankLine()
-        .append(nullableItemsListWrappers())
         .appendSingleBlankLine()
         .append(unmapListMethods())
         .appendSingleBlankLine()
