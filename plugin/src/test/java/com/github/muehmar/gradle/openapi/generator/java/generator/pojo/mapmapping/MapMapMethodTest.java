8ca1c73772eb720cb2109a55aaf323f0919292a9
Martin Mühlemann <poelsi@gmail.com>
2024-09-22 07:20:08 +0200

    Implement map mapping methods
    Issue: #160

---
+++ plugin/src/test/java/com/github/muehmar/gradle/openapi/generator/java/generator/pojo/mapmapping/MapMapMethodTest.java
@@ -0,0 +1,26 @@
+package com.github.muehmar.gradle.openapi.generator.java.generator.pojo.mapmapping;
+
+import static com.github.muehmar.gradle.openapi.generator.java.generator.data.VoidData.noData;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.data.VoidData.noSettings;
+import static com.github.muehmar.gradle.openapi.generator.java.generator.pojo.mapmapping.MapMapMethod.mapMapMethod;
+import static com.github.muehmar.gradle.openapi.snapshot.SnapshotUtil.writerSnapshot;
+import static io.github.muehmar.codegenerator.writer.Writer.javaWriter;
+
+import au.com.origin.snapshots.Expect;
+import com.github.muehmar.gradle.openapi.snapshot.SnapshotTest;
+import io.github.muehmar.codegenerator.Generator;
+import io.github.muehmar.codegenerator.writer.Writer;
+import org.junit.jupiter.api.Test;
+
+@SnapshotTest
+class MapMapMethodTest {
+  private Expect expect;
+
+  @Test
+  void mapMapMethod_when_called_then_correctOutput() {
+    final Generator<Void, Void> generator = mapMapMethod();
+    final Writer writer = generator.generate(noData(), noSettings(), javaWriter());
+
+    expect.toMatchSnapshot(writerSnapshot(writer));
+  }
+}
