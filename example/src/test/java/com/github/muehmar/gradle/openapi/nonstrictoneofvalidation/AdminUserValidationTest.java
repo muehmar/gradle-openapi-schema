7051ab9ee4ef27bb82df26964a001f24a9a3dd01
Martin Mühlemann <poelsi@gmail.com>
2024-10-09 07:40:08 +0200

    Add tests
    Issue: #285

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/nonstrictoneofvalidation/AdminUserValidationTest.java
@@ -0,0 +1,88 @@
+package com.github.muehmar.gradle.openapi.nonstrictoneofvalidation;
+
+import static com.github.muehmar.gradle.openapi.util.ValidationUtil.validate;
+import static com.github.muehmar.gradle.openapi.util.ViolationFormatter.formatViolations;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muehmar.gradle.openapi.util.MapperFactory;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import org.junit.jupiter.api.Test;
+
+class AdminUserValidationTest {
+  private static final ObjectMapper MAPPER = MapperFactory.mapper();
+
+  @Test
+  void validate_when_matchesUserSchema_then_noViolation() throws JsonProcessingException {
+    final AdminOrUserDto adminOrUserDto =
+        MAPPER.readValue(
+            "{\"id\":\"user-id\",\"type\":\"type\",\"username\":\"user-name\",\"age\":25,\"email\":null}",
+            AdminOrUserDto.class);
+
+    final Set<ConstraintViolation<AdminOrUserDto>> violations = validate(adminOrUserDto);
+
+    assertEquals(0, violations.size());
+    assertTrue(adminOrUserDto.isValid());
+  }
+
+  @Test
+  void validate_when_matchesUserSchemaButInvalidAge_then_violation()
+      throws JsonProcessingException {
+    final AdminOrUserDto adminOrUserDto =
+        MAPPER.readValue(
+            "{\"id\":\"user-id\",\"type\":\"type\",\"username\":\"user-name\",\"age\":200,\"email\":null}",
+            AdminOrUserDto.class);
+
+    final Set<ConstraintViolation<AdminOrUserDto>> violations = validate(adminOrUserDto);
+
+    assertEquals(
+        Arrays.asList(
+            "invalidOneOf[Admin].adminname -> must not be null",
+            "invalidOneOf[User].age -> must be less than or equal to 199",
+            "validAgainstNoOneOfSchema -> Is not valid against one of the schemas [Admin, User]"),
+        formatViolations(violations),
+        String.join("\n", formatViolations(violations)));
+    assertFalse(adminOrUserDto.isValid());
+  }
+
+  @Test
+  void validate_when_matchesNoSchema_then_violation() throws JsonProcessingException {
+    final AdminOrUserDto adminOrUserDto = MAPPER.readValue("{}", AdminOrUserDto.class);
+
+    final Set<ConstraintViolation<AdminOrUserDto>> violations = validate(adminOrUserDto);
+
+    assertEquals(
+        Arrays.asList(
+            "invalidOneOf[Admin].adminname -> must not be null",
+            "invalidOneOf[Admin].id -> must not be null",
+            "invalidOneOf[Admin].type -> must not be null",
+            "invalidOneOf[User].id -> must not be null",
+            "invalidOneOf[User].type -> must not be null",
+            "invalidOneOf[User].username -> must not be null",
+            "validAgainstNoOneOfSchema -> Is not valid against one of the schemas [Admin, User]"),
+        formatViolations(violations));
+    assertFalse(adminOrUserDto.isValid());
+  }
+
+  @Test
+  void validate_when_doesMatchBothSchemas_then_violation() throws JsonProcessingException {
+    final AdminOrUserDto adminOrUserDto =
+        MAPPER.readValue(
+            "{\"id\":\"id-123\",\"type\":\"type\",\"username\":\"user-name\",\"adminname\":\"admin-name\",\"age\":25,\"email\":null}",
+            AdminOrUserDto.class);
+
+    final Set<ConstraintViolation<AdminOrUserDto>> violations = validate(adminOrUserDto);
+
+    assertEquals(
+        Collections.singletonList(
+            "validAgainstMoreThanOneSchema -> Is valid against more than one of the schemas [Admin, User]"),
+        formatViolations(violations));
+    assertFalse(adminOrUserDto.isValid());
+  }
+}
