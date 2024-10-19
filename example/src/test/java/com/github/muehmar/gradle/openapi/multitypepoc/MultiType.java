6847ed640742bb7253be1de3fd0d1e96ed703a30
Martin Mühlemann <poelsi@gmail.com>
2024-10-19 16:48:44 +0200

    Update

--- example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/MultiType.java
+++ example/src/test/java/com/github/muehmar/gradle/openapi/multitypepoc/MultiType.java
@@ -1,6 +1,8 @@
 package com.github.muehmar.gradle.openapi.multitypepoc;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.util.List;
 import java.util.Objects;
 
 @JsonDeserialize(using = MultiTypeDeserializer.class)
@@ -8,30 +10,57 @@ public class MultiType {
   private final String stringValue;
   private final Long longValue;
   private final Boolean booleanValue;
+  private final List<String> list;
 
   private final SuperObject object;
 
-  public MultiType(String stringValue, Long longValue, Boolean booleanValue, SuperObject object) {
+  public MultiType(
+      String stringValue,
+      Long longValue,
+      Boolean booleanValue,
+      List<String> list,
+      SuperObject object) {
     this.stringValue = stringValue;
     this.longValue = longValue;
     this.booleanValue = booleanValue;
+    this.list = list;
     this.object = object;
   }
 
   public static MultiType fromString(String stringValue) {
-    return new MultiType(stringValue, null, null, null);
+    return new MultiType(stringValue, null, null, null, null);
   }
 
   public static MultiType fromLong(Long longValue) {
-    return new MultiType(null, longValue, null, null);
+    return new MultiType(null, longValue, null, null, null);
   }
 
   public static MultiType fromBoolean(Boolean booleanValue) {
-    return new MultiType(null, null, booleanValue, null);
+    return new MultiType(null, null, booleanValue, null, null);
+  }
+
+  public static MultiType fromList(List<String> list) {
+    return new MultiType(null, null, null, list, null);
   }
 
   public static MultiType fromObject(SuperObject object) {
-    return new MultiType(null, null, null, object);
+    return new MultiType(null, null, null, null, object);
+  }
+
+  @JsonValue
+  private Object getValue() {
+    if (stringValue != null) {
+      return stringValue;
+    } else if (longValue != null) {
+      return longValue;
+    } else if (booleanValue != null) {
+      return booleanValue;
+    } else if (list != null) {
+      return list;
+    } else if (object != null) {
+      return object;
+    }
+    return null;
   }
 
   @Override
@@ -42,12 +71,13 @@ public class MultiType {
     return Objects.equals(stringValue, multiType.stringValue)
         && Objects.equals(longValue, multiType.longValue)
         && Objects.equals(booleanValue, multiType.booleanValue)
+        && Objects.equals(list, multiType.list)
         && Objects.equals(object, multiType.object);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(stringValue, longValue, booleanValue, object);
+    return Objects.hash(stringValue, longValue, booleanValue, list, object);
   }
 
   @Override
@@ -60,6 +90,8 @@ public class MultiType {
         + longValue
         + ", booleanValue="
         + booleanValue
+        + ", list="
+        + list
         + ", object="
         + object
         + '}';
