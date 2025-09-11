5248e6f189d0fda061aad847107a12de0e82d9d2
Martin Mühlemann <poelis@gmail.com>
2025-09-11 21:54:28 +0200

    Add tests for 308
    #308

---
+++ example/src/test/java/com/github/muehmar/gradle/openapi/additionalproperties/MapAdditionalPropertiesTest.java
@@ -0,0 +1,53 @@
+package com.github.muehmar.gradle.openapi.additionalproperties;
+
+import static com.github.muehmar.gradle.openapi.util.ValidationUtil.validate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.muehmar.gradle.openapi.util.MapperFactory;
+import com.github.muehmar.openapi.util.AdditionalProperty;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import org.junit.jupiter.api.Test;
+
+public class MapAdditionalPropertiesTest {
+  private static final ObjectMapper MAPPER = MapperFactory.mapper();
+
+  @Test
+  void validate_when_validAdditionalProperty_then_noViolationsAndValueReturned()
+      throws JsonProcessingException {
+    final MapAdditionalPropertiesDto dto =
+        MAPPER.readValue(
+            "{\"name\":\"name\",\"data\":{\"hello\":\"world\"}}", MapAdditionalPropertiesDto.class);
+
+    assertEquals(
+        Optional.of(Collections.singletonList(new AdditionalProperty<>("hello", "world"))),
+        dto.getAdditionalProperty("data")
+            .map(MapAdditionalPropertiesPropertyDto::getAdditionalProperties));
+
+    final Set<ConstraintViolation<MapAdditionalPropertiesDto>> violations = validate(dto);
+
+    assertEquals(0, violations.size());
+    assertTrue(dto.isValid());
+  }
+
+  @Test
+  void serialize_when_withArrayAsAdditionalProperty_then_correctJson()
+      throws JsonProcessingException {
+    final MapAdditionalPropertiesDto dto =
+        MapAdditionalPropertiesDto.builder()
+            .setName("name")
+            .andAllOptionals()
+            .addAdditionalProperty(
+                "data",
+                new MapAdditionalPropertiesPropertyDto(Collections.singletonMap("hello", "world")))
+            .build();
+
+    final String json = MAPPER.writeValueAsString(dto);
+    assertEquals("{\"name\":\"name\",\"data\":{\"hello\":\"world\"}}", json);
+  }
+}
